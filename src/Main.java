de96567238a2955702786f5b5b8472ef9b60db0f
mambatukaa <sbatuka9@gmail.com>
2023-03-03 16:57:12 -0800

    solve ConcatenationOfArray

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import LeetCode.BuildArrayFromPermutation;
+import LeetCode.ConcatenationOfArray;
 
 import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] nums = {1, 3};
 
-        BuildArrayFromPermutation buildArrayFromPermutation = new BuildArrayFromPermutation(nums);
+        ConcatenationOfArray concatenationOfArray = new ConcatenationOfArray(nums);
 
-        System.out.println(Arrays.toString(buildArrayFromPermutation.solution()));
+        System.out.println(Arrays.toString(concatenationOfArray.solution()));
 
     }
 
