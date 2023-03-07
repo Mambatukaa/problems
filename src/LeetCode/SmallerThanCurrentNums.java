268b149c40a2f86cda2d1cd8a09ab775293f0095
mambatukaa <sbatuka9@gmail.com>
2023-03-06 17:30:48 -0800

    add max upvoted solution

--- src/LeetCode/SmallerThanCurrentNums.java
+++ src/LeetCode/SmallerThanCurrentNums.java
@@ -51,4 +51,30 @@ public class SmallerThanCurrentNums {
         return nums;
     }
 
+    public int[] solutionFromLeetCode() {
+        int[] count = new int[101];
+        int[] res = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+
+        System.out.println(Arrays.toString(count));
+
+        for (int i = 1; i <= 100; i++) {
+            count[i] += count[i - 1];
+        }
+
+        System.out.println(Arrays.toString(count));
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                res[i] = 0;
+            else
+                res[i] = count[nums[i] - 1];
+        }
+
+        return res;
+    }
+
 }
