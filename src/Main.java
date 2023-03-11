5e7b3bb73f3f395f39dd1a5fa097aeeaafcffb8f
mambatukaa <sbatuka9@gmail.com>
2023-03-10 16:35:24 -0800

    add solution of valid mountain array

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,11 @@ import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        int[] arr = {-2, 0, 10, -19, 4, 6, -8};
-//        int[] arr = {10, 2, 5, 3};
-//        int[] arr = {0, 0};
+        int[] arr = {3, 6, 5, 6, 7, 6, 5, 3, 0};
 
-// [-2,0,10,-19,4,6,-8]
+        ValidMountainArray validMountainArray = new ValidMountainArray(arr);
 
-        CheckNDoubleExist checkNDoubleExist = new CheckNDoubleExist(arr);
-
-        System.out.println(checkNDoubleExist.bruteForceSolution());
-
-
+        System.out.println(validMountainArray.solution());
     }
 
 }
