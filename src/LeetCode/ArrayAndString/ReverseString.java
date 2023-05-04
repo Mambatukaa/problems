1b25009090db565d3234b8fda3ba5c239df3dde1
mambatukaa <sbatuka9@gmail.com>
2023-05-04 10:29:21 -0700

    naive solution for reverse string

--- src/LeetCode/ArrayAndString/ReverseString.java
+++ src/LeetCode/ArrayAndString/ReverseString.java
@@ -1,5 +1,7 @@
 package LeetCode.ArrayAndString;
 
+import java.util.Arrays;
+
 public class ReverseString {
     final private char[] s;
 
@@ -7,7 +9,29 @@ public class ReverseString {
         this.s = s;
     }
 
-    public void naive() {
+    public void swap(char[] s, int left, int right) {
+        char temp = s[left];
 
+        s[left] = s[right];
+        s[right] = temp;
     }
+
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    public void naive() {
+        int left = 0;
+        int right = s.length - 1;
+
+
+        while (left < right) {
+            swap(s, left, right);
+
+            left++;
+            right--;
+        }
+
+        System.out.printf(Arrays.toString(s));
+    }
+
 }
