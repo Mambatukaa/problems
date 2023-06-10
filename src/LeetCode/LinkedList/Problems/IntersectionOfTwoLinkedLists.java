383ecba7aa304af9c96edb834d72a4ab978b68bd
mambatukaa <sbatuka9@gmail.com>
2023-06-10 09:24:53 -0700

    add two pointers solution for intersection of two linked lists

--- src/LeetCode/LinkedList/Problems/IntersectionOfTwoLinkedLists.java
+++ src/LeetCode/LinkedList/Problems/IntersectionOfTwoLinkedLists.java
@@ -25,4 +25,21 @@ public class IntersectionOfTwoLinkedLists {
 
         return null;
     }
+
+
+    // Time complexity: O(N+M)
+    // Space complexity: O(1)
+    // Solution for two pointers
+    public ListNode solution(ListNode headA, ListNode headB) {
+        ListNode ptr1 = headA;
+        ListNode ptr2 = headB;
+
+
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1 == null ? headB : ptr1.next;
+            ptr2 = ptr2 == null ? headA : ptr2.next;
+        }
+
+        return ptr1;
+    }
 }
