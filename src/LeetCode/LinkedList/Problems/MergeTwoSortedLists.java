308a54332697c64f82e6c73568a8c91e34b4dfa8
mambatukaa <sbatuka9@gmail.com>
2023-06-22 12:47:43 -0700

    add video solution for merge two sorted lists

--- src/LeetCode/LinkedList/Problems/MergeTwoSortedLists.java
+++ src/LeetCode/LinkedList/Problems/MergeTwoSortedLists.java
@@ -62,6 +62,63 @@ public class MergeTwoSortedLists {
     }
 
 
+    // video solution
+    // Time complexity: O(n + m)
+    // Space complexity: O(1)
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = list1 == null ? list2 : list1;
+
+
+        return head.next;
+    }
+
+    // video solution
+    // recursive
+    // Time complexity: O(n+m);
+    // Space compleixty: O(n+m);
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        // base case
+        if (list1 == null) {
+            return list2;
+        }
+        ;
+        if (list2 == null) {
+            return list1;
+        }
+        ;
+
+        ListNode head;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+
+        head.next = mergeTwoLists(list1, list2);
+
+        return head;
+    }
+
     public void traversal(ListNode head) {
         ListNode temp = head;
 
