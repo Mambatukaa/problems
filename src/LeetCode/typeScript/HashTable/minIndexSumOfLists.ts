5e85f1504f6ec4d9dd114e694c02a59ff1dea74f
mambatukaa <sbatuka9@gmail.com>
2023-07-31 11:09:53 -0700

    add solution for minIndexSumOfLists using hashMap

--- src/LeetCode/typeScript/HashTable/minIndexSumOfLists.ts
+++ src/LeetCode/typeScript/HashTable/minIndexSumOfLists.ts
@@ -1,4 +1,4 @@
-function minIndex(list1: string[], list2: string[]): string[] {
+function minIndex1(list1: string[], list2: string[]): string[] {
   let n = 10000;
   let obj: any ={} ;
 
@@ -36,9 +36,48 @@ function minIndex(list1: string[], list2: string[]): string[] {
   return data;
 }
 
+// Time Complexity: O(l1 + l2);
+// Space Complexity: O(l1 * x); hashmap size grows upto l1∗x, where x refers to average string length;
 
-const list1 = ["happy","sad","good"], list2 = ["sad","happy","good"];
+function findMinIndex(list1: string[], list2: string[]): string[] {
+  if(list1.length < list2.length) {
+    return findMinIndex(list2, list1);
+  }
 
-// "1"
+  let minIndex = 10000;
+  const map = new Map();
+  let result: string[] = [];
 
-console.log(minIndex(list1, list2));
+  for(let i = 0; i < list1.length; i++) {
+    map.set(list1[i], i);
+  }
+
+
+  for(let i = 0; i < list2.length; i++) {
+    const word = list2[i];
+
+    if(map.has(word)) {
+      const curMinIndex = map.get(word) + i;
+
+      if(curMinIndex < minIndex) {
+        minIndex = curMinIndex;
+
+        result = [];
+
+        result.push(word);
+      } else if(curMinIndex === minIndex) {
+        result.push(word);
+      }
+
+    }
+
+  }
+
+  return result; 
+}
+
+
+const list1 = ["happy","ball","good", "basket"], list2 = ["ball","happy","good"];
+
+
+console.log(findMinIndex(list1, list2));
