ea61155156020b6a3d6b25ef410efba7332c2479
mambatukaa <sbatuka9@gmail.com>
2023-08-01 11:38:04 -0700

    add solution using hashSet

--- src/LeetCode/typeScript/HashTable/containsDuplicateII.ts
+++ src/LeetCode/typeScript/HashTable/containsDuplicateII.ts
@@ -1,3 +1,4 @@
+// Using HashMap
 // Time complexity: O(n)
 // Space complexity: O(n)
 
@@ -17,4 +18,28 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
   return false;
 }
 
-console.log(containsNearbyDuplicate([1,2,3,4,5,1, 1], 3));
+
+// using HashSet
+function containsNearbyDuplicate1(nums: number[], k: number): boolean {
+  const set = new Set();
+
+  for(let i = 0; i < nums.length; i++) {
+    if(set.has(nums[i])) {
+      console.log("Number: ", nums[i]);
+      return true;
+    }
+
+    set.add(nums[i]);
+
+    if(set.size > k) {
+      set.delete(nums[i - k]);
+    }
+
+    console.log(set, set.size);
+
+  }
+
+  return false;
+}
+
+console.log(containsNearbyDuplicate1([1,2,3,4,5,1,2,3,4,5,1,1], 3));
