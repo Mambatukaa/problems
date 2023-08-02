137e13f6d29872b570068e532df7191f01ddf8b4
mambatukaa <sbatuka9@gmail.com>
2023-08-02 11:31:53 -0700

    add solution for isValidSudoku using arr index

--- src/LeetCode/typeScript/HashTable/validSudoku.ts
+++ src/LeetCode/typeScript/HashTable/validSudoku.ts
@@ -1,6 +1,5 @@
-// Time Complexity: O(n*2) because we need to traverse every position in the board, and each of the four check steps is an O(1)O(1)O(1) operation.
-// Space Complexity: O(n*2) because in the worst-case scenario, if the board is full, we need a hash set each with size N to store all seen numbers for each of the N rows, N columns, and N boxes, respectively.
-//
+// Time Complexity: O(n^2) because we need to traverse every position in the board, and each of the four check steps is an O(1)O(1)O(1) operation.
+// Space Complexity: O(n^2) because in the worst-case scenario, if the board is full, we need a hash set each with size N to store all seen numbers for each of the N rows, N columns, and N boxes, respectively.
 function isValidSudoku(board: string[][]) {
   const N = 9;
 
@@ -44,6 +43,49 @@ function isValidSudoku(board: string[][]) {
   return true;
 }
 
+// Time Complexity: O(n^2);
+// Space Complexity: O(n^2)
+function isValidSudokuArr(board: string[][]) {
+  const N = 9;
+
+  const cols = new Array(9).fill(null).map(() => new Array(N));
+  const rows = new Array(9).fill(null).map(() => new Array(N));
+  const boxes = new Array(9).fill(null).map(() => new Array(N));
+
+  for(let r = 0; r < N; r++) {
+    for(let c = 0; c < N; c++) {
+      if(board[r][c] === '.') {
+        continue;
+      }
+
+      const pos = Number(board[r][c]) - 1;
+
+      if(cols[c][pos] === 1) {
+        return false;
+      }
+
+      cols[c][pos] = 1;
+
+      if(rows[r][pos] === 1) {
+        return false;
+      }
+
+      rows[r][pos] = 1;
+
+      const idx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+      if(boxes[idx][pos] === 1) {
+        return false;
+      }
+
+      boxes[idx][pos] = 1;
+    }
+
+  }
+
+  return true;
+}
+
 
 const board = 
 [["5","3",".",".","7",".",".",".","."]
@@ -57,4 +99,4 @@ const board =
 ,[".",".",".",".","8",".",".","7","9"]]
 
 
-console.log(isValidSudoku(board));
+console.log(isValidSudokuArr(board));
