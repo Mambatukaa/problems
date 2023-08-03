5fa88c66f1261581ae0015e2ef73f9a03c887f6e
mambatukaa <sbatuka9@gmail.com>
2023-08-03 12:50:36 -0700

    add solution for preOrderTraversal using iteration

--- src/LeetCode/typeScript/binaryTree/preOrderTraversal.ts
+++ src/LeetCode/typeScript/binaryTree/preOrderTraversal.ts
@@ -25,6 +25,28 @@ function preOrderTraversal(root: ITreeNode) {
   return answer;
 }
 
+// Time complexity: O(n)
+// Space complexity: O(n)
+function preOrderTraversalIteration(root: ITreeNode | null) {
+  const stack: Array<TreeNode | null> = [];
+  const answer: Array<number> = [];
+
+  stack.push(root);
+
+  while(stack.length) {
+    const currNode = stack.pop();
+
+    if(currNode) {
+      answer.push(currNode.val);
+      stack.push(currNode.right);
+      stack.push(currNode.left);
+    }
+
+  }
+
+  return answer;
+}
+
 // Definition for a binary tree node.
 class TreeNode {
     val: number
@@ -39,6 +61,31 @@ class TreeNode {
 
 
 const node1 = new TreeNode(1);
+const node2 = new TreeNode(2);
+const node3 = new TreeNode(3);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5);
+const node6 = new TreeNode(6);
+const node7 = new TreeNode(7);
+const node8 = new TreeNode(8);
+const node9 = new TreeNode(9);
+const node10 = new TreeNode(10);
+
+node1.left = node2;
+node1.right = node3;
+
+node2.left = node4;
+node2.right = node5;
+
+
+node3.left = node6;
+node3.right = node7;
+
+node4.left = node8;
+node4.right = node9;
+
+node5.left = node10;
 
 
 console.log(preOrderTraversal(node1));
+console.log(preOrderTraversalIteration(node1));
