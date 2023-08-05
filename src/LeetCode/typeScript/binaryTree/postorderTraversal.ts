e29f69c2555b9389697fc9b11d83925495f55bc3
mambatukaa <sbatuka9@gmail.com>
2023-08-05 10:50:54 -0700

    add solution to postorderTraversal using recursive

---
+++ src/LeetCode/typeScript/binaryTree/postorderTraversal.ts
@@ -0,0 +1,36 @@
+import { TreeNode } from './TreeNode';
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function postorderTraversal(root: TreeNode | null) {
+  const answer: number[] = [];
+
+  function postorder(root: TreeNode | null) {
+    if(root === null) {
+      return [];
+    }
+
+    postorder(root.left);
+    postorder(root.right);
+    answer.push(root.val);
+  }
+
+  postorder(root);
+
+  return answer;
+}
+
+
+const node1 = new TreeNode(1);
+const node2 = new TreeNode(2);
+const node3 = new TreeNode(3);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5);
+
+node1.left = node2;
+node1.right = node3;
+
+node2.left = node4;
+node2.right = node5
+
+console.log(postorderTraversal(node1));
