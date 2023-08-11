1f9df2e286781861769ff41b37b0fd4b640c089f
mambatukaa <sbatuka9@gmail.com>
2023-08-11 09:40:14 -0700

    add solution to pathSum using iteration

--- src/LeetCode/typeScript/binaryTree/pathSum.ts
+++ src/LeetCode/typeScript/binaryTree/pathSum.ts
@@ -19,13 +19,53 @@ const hasPathSum = (root: TreeNode | null, sum: number, pos?: string): boolean =
 }
 
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const hasPathSumIteration = (root: TreeNode | null, sum: number): boolean => {
+  if(!root) {
+    return false;
+  }
+
+  const nodeStack: any[] = [];
+  const sumStack: number[] = [];
+
+  nodeStack.push(root);
+  sumStack.push(sum - root.val);
+
+  while(nodeStack.length) {
+    const node = nodeStack.pop();
+    const currSum: any = sumStack.pop();
+
+    console.log(node.val, ' value');
+    console.log(currSum, ' sum');
+
+    if(!node.right && !node.left && currSum === 0) {
+      return true;
+    }
+
+    if(node.right) {
+      nodeStack.push(node.right);
+      sumStack.push(currSum - node.right.val);
+    }
+
+    if(node.left) {
+      nodeStack.push(node.left);
+      sumStack.push(currSum - node.left.val);
+    }
+
+  }
+
+  return false;
+}
+
 const node1 = new TreeNode(1);
-const node2 = new TreeNode(2);
-const node3 = new TreeNode(3);
-const node4 = new TreeNode(4);
-const node5 = new TreeNode(5);
-const node6 = new TreeNode(6);
-const node7 = new TreeNode(7);
+const node2 = new TreeNode(-2);
+const node3 = new TreeNode(-3);
+const node4 = new TreeNode(1);
+const node5 = new TreeNode(3);
+const node6 = new TreeNode(-2);
+const node7 = new TreeNode(-1);
+
 
 node1.left = node2;
 node1.right = node3;
@@ -33,9 +73,9 @@ node1.right = node3;
 node2.left = node4;
 node2.right = node5;
 
+node3.left= node6;
 
-node3.left = node6;
-node3.right = node7;
+node4.left = node7;
 
 
-console.log(hasPathSum(node1, 1111));
+console.log(hasPathSumIteration(node1, -1));
