0c0d672bef30fa65ff991b5c4cf71a56b789b1af
mambatukaa <sbatuka9@gmail.com>
2023-08-16 12:02:59 -0700

    add solution which has no global variable

--- typeScript/binaryTree/problems/countUnivalueSubtrees.ts
+++ typeScript/binaryTree/problems/countUnivalueSubtrees.ts
@@ -39,6 +39,7 @@ const countUnivals = (root: TreeNode | null): number => {
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+// Solution with global variable
 const solution = (root: TreeNode | null): number  => {
   let count: number = 0;
 
@@ -71,6 +72,86 @@ const solution = (root: TreeNode | null): number  => {
   return count;
 }
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// No global variable
+const solution1 = (root: TreeNode | null) => {
+  const dfs = (node: TreeNode | null) => {
+    if(!node) {
+      return {
+        key: true,
+        value: 0
+      }
+    }
+
+    const left: {key: boolean, value: number} = dfs(node.left);
+    const right: {key: boolean, value: number} = dfs(node.right);
+
+    const isLeftUniValue: boolean = left.key;
+    const isRightUniValue: boolean = right.key;
+
+    const count: number = left.value + right.value;
+
+    if(isLeftUniValue && isRightUniValue) {
+      if(node.left && node.left.val !== node.val) {
+        return {
+          key: false,
+          value: count
+        }
+      }
+
+      if(node.right && node.right.val !== node.val) {
+        return {
+          key: false,
+          value: count
+        }
+      }
+
+      return {
+        key: true,
+        value: count + 1
+      }
+
+    }
+
+    return {
+      key: false,
+      value: count
+    }
+  }
+
+
+  return dfs(root).value;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// No global variable
+const solution2 = (root: TreeNode | null): number => {
+  const count = [0];
+
+  const dfs = (node: TreeNode | null, count: number[]): boolean => {
+    if(!node) {
+      return true;
+    }
+
+    const left: boolean = dfs(node.left, count);
+    const right: boolean = dfs(node.right, count);
+
+    if(left && right && (!node.left || node.left.val === node.val) && (!node.right || node.right.val === node.val) ) {
+      count[0]++;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  dfs(root, count);
+
+  return count[0];
+}
+
 
 const node1 = new TreeNode(1);
 const node2 = new TreeNode(1);
@@ -87,4 +168,4 @@ node2.right = node5;
 
 node3.right = node6;
 
-console.log(solution(node1));
+console.log(solution2(node1));
