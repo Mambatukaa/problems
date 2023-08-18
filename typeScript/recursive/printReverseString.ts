c87b5f80ba356a59238a14417bfcce8da0fba55a
mambatukaa <sbatuka9@gmail.com>
2023-08-17 21:34:35 -0700

    add solution to reverse String using recursion

--- typeScript/recursive/printReverseString.ts
+++ typeScript/recursive/printReverseString.ts
@@ -28,4 +28,32 @@ const reverseString = (s: string[]): void => {
 }
 
 
-reverseString(["h", "e", "l", "l", "o", "o"]);
+// using two pointers
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const reverseStringRecursive = (s: string[]): void => {
+  let leftIndex = 0;
+  let rightIndex = s.length - 1;
+
+  const reverse = (s: string[], leftIndex: number, rightIndex: number) => {
+    if(leftIndex >= rightIndex) {
+      return;
+    }
+
+    const temp = s[leftIndex];
+    s[leftIndex] = s[rightIndex];
+    s[rightIndex] = temp;
+
+    reverse(s, leftIndex + 1, rightIndex - 1);
+  }
+
+
+  reverse(s, leftIndex, rightIndex);
+
+}
+
+const s = ["h", "e", "l", "l", "o", "o"];
+
+reverseStringRecursive(s);
+
+console.log('updated string: ', s);
