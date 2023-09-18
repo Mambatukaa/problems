1eeb66ad1aacc772addacd1d7789a4af4852a73c
mambatukaa <sbatuka9@gmail.com>
2023-09-18 10:41:01 -0700

    add solution for validate subsequence

--- js/validateSubsequence.js
+++ js/validateSubsequence.js
@@ -1,12 +1,34 @@
 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 function isValidSubsequence(array, sequence) {
-  // [1,3,4]
-  // [1,2,3,4]
+  let index = 0;
 
-  const map = {};
+  for(let i = 0; i < array.length; i++) {
+    const currNum = array[i];
+    const sequenceNum = sequence[index];
 
-  for(let i = 0; i < sequence.length; i++) {
-
+    if(sequenceNum === currNum) {
+      index++;
+    }
   }
 
+  return index === sequence.length;
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+function isValidSubsequenceII(array, sequence) { 
+
+  for(let i = array.length - 1; i >= 0; i--) {
+    if(sequence[sequence.length - 1] === array[i]) {
+      sequence.pop();
+    }
+  }
+
+  console.log(sequence);
+
+  return !sequence.length;
+}
+
+console.log(isValidSubsequenceII([1,1,1,1,1], [1,1,1]));
