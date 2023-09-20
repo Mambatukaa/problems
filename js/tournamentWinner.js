9ea22512c97ee70058413dc9f8393ed449e96b7a
mambatukaa <sbatuka9@gmail.com>
2023-09-19 22:44:28 -0700

    refactor

--- js/tournamentWinner.js
+++ js/tournamentWinner.js
@@ -1,10 +1,12 @@
 
 const getWinnerIndex = (idx) => idx === 0 ? 1 : 0; 
 
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+// Time Complexity: O(n) n is competitions
+// Space Complexity: O(k) k is teams
 const tournamentWinner = (compeitions, results) => {
   const map = new Map();
+  let bestTeam = '';
+  map.set('bestTeam', 0);
 
   for(let i = 0; i < competitions.length; i++) {
     const game = competitions[i];
@@ -15,24 +17,21 @@ const tournamentWinner = (compeitions, results) => {
     } else {
       map.set(winner, map.get(winner) + 3);
     }
-  }
+
+    const bestTeamScore = map.get('bestTeam');
+    const winnerScore = map.get(winner);
 
 
-  let bestScore = 0;
-  let winner = '';
+    if(bestTeamScore < winnerScore) {
+      bestTeam = winner;
 
-  for(let entry of map.entries()) {
-    const team = entry[0];
-    const teamScore = entry[1];
-
-    if(teamScore > bestScore) {
-      bestScore = teamScore;
-      winner = team
+      map.set('bestTeam', winnerScore);
     }
+
+    console.log(map)
   }
 
-
-  return winner;
+  return bestTeam;
 }
 
 const competitions = [
