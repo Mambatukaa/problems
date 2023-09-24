8e451f7ca82917b35fb5cf5e09712a08089208c1
mambatukaa <sbatuka9@gmail.com>
2023-09-23 23:51:09 -0700

    add iterative solution

--- js/branchSums.js
+++ js/branchSums.js
@@ -25,6 +25,38 @@ const solution = (root, sum) => {
   return answer;
 }
 
+// Pre-order traversal // loop
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const solutionII = (root) => {
+  let sum = root.value;
+  let curr = root;
+  const answer = [];
+
+  const stack = [[curr, sum]];
+
+  while(stack.length) {
+    [curr, sum] = stack.pop();
+
+
+    if(!curr.left && !curr.right) {
+      answer.push(sum);
+      continue;
+    }
+
+    if(curr.right) {
+      stack.push([curr.right, sum + curr.right.value]);
+    }
+
+    if(curr.left) {
+      stack.push([curr.left, sum + curr.left.value]);
+    }
+
+  }
+
+  return answer;
+}
+
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -63,4 +95,4 @@ node4.right = node9;
 
 node5.left = node10;
 
-console.log(solution(root));
+console.log(solutionII(root));
