ee21609406f5722d9769ee7d5221f2736f589aac
mambatukaa <sbatuka9@gmail.com>
2023-09-30 14:24:36 -0700

    add solution for the minWaitingTime

--- js/minWaitingTime.js
+++ js/minWaitingTime.js
@@ -1,8 +1,53 @@
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
 const minWaitingTime = (queries) => {
+  let waitingTime = 0;
+  let executionTime = 0;
 
+  queries.sort((a, b) => a - b);
+
+  for(let num of queries) {
+    waitingTime += executionTime; 
+
+    executionTime = executionTime + num;
+
+  }
+
+  return waitingTime;
 }
 
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+const minWaitingTimeII = (queries) => {
+  queries.sort((a, b) => a - b);
 
-const queries = [5, 1, 4];
+  let waitingTime = 0;
 
-minWaitingTime(queries);
+  for(let i = 0; i < queries.length; i++) {
+    let queriesLeft = queries.length - (i + 1);
+
+    console.log(queries[i], queriesLeft, waitingTime);
+
+    waitingTime += queries[i] * queriesLeft;
+
+
+  }
+
+
+  return waitingTime;
+}
+
+const queries = [1,2,3];
+
+minWaitingTimeII(queries);
+
+  // [1,2,3] ==> 0 + 1 + (1 + 2)
+  // [3,2,1] ==> 0 + 3 + (3 + 2)
+  // [3,2,1,2,6] => 0 + 3 + (3 + 2) + (3 + 2 + 1) + (3 + 2 + 1 + 2) = 22
+  // [1,2,2,3,5] => 0 + 1 + (1 + 2) + (1 + 2 + 2) + (1 + 2 + 2 + 3) = 17
+// 1 -> 4 -> 9 -> 17
+//
+//
+//
+//
+// 1,2,3 => 0 + 1 + (1 + 2) + (1 + 2 + 3)
