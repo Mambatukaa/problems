de016bf576eee4287d840480f9e1c6bceaf2ee39
mambatukaa <sbatuka9@gmail.com>
2023-10-02 07:57:03 -0700

    initial commit

---
+++ js/optimalFreelancing.js
@@ -0,0 +1,12 @@
+const optimalFreelancing = (jobs) => {
+
+}
+
+
+const jobs = [
+  {deadline: 1, payment: 1},
+  {deadline: 2, payment: 1},
+  {deadline: 2, payment: 2}
+];
+
+optimalFreelancing(jobs);
