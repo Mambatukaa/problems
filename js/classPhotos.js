3c28b8b228d3c71bebd48e058a56a783fea5bf87
mambatukaa <sbatuka9@gmail.com>
2023-10-03 22:32:29 -0700

    update

--- js/classPhotos.js
+++ js/classPhotos.js
@@ -21,8 +21,32 @@ const classPhotos = (firstRow, secondRow) => {
   return true;
 }
 
+// Time Complexity: O(nlog*n)
+// Space Complexity: O(1)
+const classPhotosII = (blueShirtHeights, redShirtHeights) => {
+  blueShirtHeights.sort((a,b) => a - b);
+  redShirtHeights.sort((a,b) => a - b);
+
+  let blueBack = true;
+  let redBack = true;
+
+  for(let i = 0; i < blueShirtHeights.length; i++) {
+    if(blueShirtHeights[i] <= redShirtHeights[i]) {
+      blueBack = false;
+    }
+
+    if(blueShirtHeights[i] >= redShirtHeights[i]) {
+      redBack = false;
+    }
+
+  }
+
+  return redBack || blueBack;
+}
+
+
 const blueShirt = [1,2,3];
 const redShirt = [2,3,4];
 
 
-console.log(classPhotos(blueShirt, redShirt));
+console.log(classPhotosII(blueShirt, redShirt));
