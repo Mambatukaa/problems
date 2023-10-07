80887780b80debe85b60e980e4e9545fa4a0d0b0
mambatukaa <sbatuka9@gmail.com>
2023-10-07 11:15:35 -0700

    update

--- js/nthFibonacci.js
+++ js/nthFibonacci.js
@@ -12,5 +12,31 @@ const fib = (n) => {
   return fib(n - 1) + fib(n - 2);
 }
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const fibI = (n, memoize = { 1: 0, 2: 1 }) => {
+  if(n in memoize) {
+    return memoize[n]
+  }
 
-console.log(fib(6));
+  memoize[n] = fibI(n - 1) + fibI(n - 2);
+
+  return memoize[n];
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const fibII = (n) => {
+  let first = 0;
+  let second = 1;
+
+  for(let i = 1; i < n; i++) {
+    const temp = first;
+    first = second;
+    second = temp + second;
+  }
+
+  return first;
+}
+
+console.log(fibII(5));
