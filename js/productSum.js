4f2aff2023947013c37b6bfd71b477252bc7eb6a
mambatukaa <sbatuka9@gmail.com>
2023-10-09 10:43:27 -0700

    add solution for product sum using recursion

--- js/productSum.js
+++ js/productSum.js
@@ -1,7 +1,23 @@
-const productSum = (arr) => {
+// Time Complexity: O(n) 
+// Space Complexity: O(d) 
+// n is the total number of elements in the array
+// d is greatest depth of "special" arrays
+const productSum = (item, level = 1) => {
+  let sum = 0;
 
+  for(let el of item) {
+    if(Array.isArray(el)) {
+      sum += productSum(el, level + 1);
+    } else {
+      sum += el;
+    }
+  }
+
+  console.log(sum);
+
+  return level * sum;
 }
 
 
 
-productSum([1, 2, [1, 1]]);
+console.log(productSum([0, 2, [3,4]]));
