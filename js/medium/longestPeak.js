9f5070f5931a89a0eddae2b92c5c56e0f03f7ef5
mambatukaa <sbatuka9@gmail.com>
2023-10-26 22:50:23 -0700

    add clean solution

--- js/medium/longestPeak.js
+++ js/medium/longestPeak.js
@@ -37,6 +37,44 @@ const longestPeak = (array) => {
 }
 
 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const longestPeakII = (array) => {
+  let longestPeak = 0;
+  let i = 1;
+
+  while(i < array.length - 1) {
+    const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+
+    if(!isPeak) {
+      i++;
+      continue;
+    }
+
+    let leftIndex = i - 2;
+
+    while(leftIndex >= 0 && array[leftIndex] < array[leftIndex + 1]) {
+      leftIndex--;
+    }
+
+    let rightIndex = i + 2;
+
+    while(rightIndex < array.length && array[rightIndex] < array[rightIndex - 1]) {
+      rightIndex++;
+    }
+
+    const currentPeakLength = rightIndex - leftIndex - 1;
+
+    longestPeak = Math.max(longestPeak, currentPeakLength);
+
+    i = rightIndex;
+  }
+
+
+  return longestPeak;
+}
+
+
 const array = [1,2,3,3,4,0,10,6,5,-1,-3,2,3];
 // output = 6 // 0,10,6,5,-1,-3
-console.log(longestPeak(array));
+console.log(longestPeakII(array));
