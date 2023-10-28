0d315c78467a4f34e04046bb4c51c3fae426159e
mambatukaa <sbatuka9@gmail.com>
2023-10-27 22:11:12 -0700

    add optimal solution

--- js/medium/arrayOfProducts.js
+++ js/medium/arrayOfProducts.js
@@ -26,7 +26,41 @@ const arrayOfProducts = (array) => {
   return answer;
 }
 
+// Space Complexity: O(1)
+// Time Complexity: O(n)
+const arrayOfProductsII = (array) => {
+  let zeroCounter = 0;
+  let total = 1;
 
-const array = [0,0,0,0];
+  for(const num of array) {
+    if(num === 0) {
+      zeroCounter++;
 
-console.log(arrayOfProducts(array));
+      continue;
+    }
+
+    total *= num;
+  }
+
+
+  if(zeroCounter > 1) {
+    return array.fill(0);
+  }
+
+  for(let i = 0; i < array.length; i++) {
+    if(zeroCounter === 1 && array[i] === 0) {
+      array.fill(0);
+      array[i] = total;
+      break;
+    }
+
+    array[i] = total / array[i];
+  }
+
+  return array;
+}
+
+
+const array = [2,1,2,3];
+
+console.log(arrayOfProductsII(array));
