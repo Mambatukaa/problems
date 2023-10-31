5ef366807503e74d67ef399bb5c345a0cfea2424
mambatukaa <sbatuka9@gmail.com>
2023-10-31 09:22:52 -0700

    update

--- js/medium/firstDuplicateValue.js
+++ js/medium/firstDuplicateValue.js
@@ -1,3 +1,22 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+const firstDuplicateValue = (array) => {
+  let minSecondIndex = array.length;
+
+  for(let i = 0; i < array.length; i++) {
+
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[i] === array[j]) {
+        minSecondIndex = Math.min(minSecondIndex, j);
+      }
+    }
+  }
+
+  if(minSecondIndex === array.length) return -1;
+
+  return array[minSecondIndex];
+}
+
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 const firstDuplicateValueII = (array) => { 
@@ -14,5 +33,22 @@ const firstDuplicateValueII = (array) => {
   return -1;
 }
 
+// work only between 1 and n
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const firstDuplicateValueIII = (array) => { 
 
-console.log(firstDuplicateValueII([2,1,4,2,3,3,4]));
+  for(const num of array) {
+    const absValue = Math.abs(num);
+
+    if(array[absValue - 1] < 0) return absValue;
+
+    array[absValue - 1] *= -1;
+
+    console.log(array)
+  }
+
+  return -1;
+}
+
+console.log(firstDuplicateValueIII([1,20,100,100]));
