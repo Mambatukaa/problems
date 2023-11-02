ac9bf7a015b5c7e9264eb973f153a7d6ef5c4f3c
mambatukaa <sbatuka9@gmail.com>
2023-11-02 11:54:54 -0700

    add solution for zeroSumSubarray

---
+++ js/medium/zeroSumSubarray.js
@@ -0,0 +1,24 @@
+
+// Space Complexity: O(n)
+// Time Complexity: O(n)
+const zeroSumSubArray = (nums) => {
+  let total = 0;
+  const sums = new Set();
+  sums.add(0)
+
+  for(const num of nums) {
+    total += num;
+
+    if(sums.has(total)) {
+      return true;
+    }
+
+    sums.add(total);
+  }
+
+  return false;
+}
+
+const nums = [-8,-5,-3,0,1,10,4];
+
+console.log(zeroSumSubArray(nums));
