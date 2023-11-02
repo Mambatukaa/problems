0f9414dfffa8d8fbd1d5fb11d6c7d15161380c08
mambatukaa <sbatuka9@gmail.com>
2023-11-02 12:44:55 -0700

    update

--- js/medium/missingNumbers.js
+++ js/medium/missingNumbers.js
@@ -4,7 +4,6 @@ const missingNumbers = (nums) => {
   const output = [];
   const array = new Array(nums.length + 2).fill(1);
 
-
   for(const num of nums) {
     array[num-1] = -1;
   }
@@ -14,6 +13,19 @@ const missingNumbers = (nums) => {
       output.push(i+1);
     }
 
+  }
+
+  return output;
+}
+
+const missingNumbersII = (nums) => {
+  const output = [];
+  const set = new Set(nums);
+
+  for(let num = 1; num < nums.length + 3; num++) {
+    if(!set.has(num)) {
+      output.push(num);
+    }
   }
 
   return output;
