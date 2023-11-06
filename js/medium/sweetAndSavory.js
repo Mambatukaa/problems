c75642fa8aa58d47c8d4e27ddfee7af5be45c732
mambatukaa <sbatuka9@gmail.com>
2023-11-06 10:06:07 -0800

    add optimal solution for sweetAndSavory

--- js/medium/sweetAndSavory.js
+++ js/medium/sweetAndSavory.js
@@ -1,10 +1,40 @@
-const sweetAndSavory(dishes, target) {
+// Time Complexity: O(nlog(n))
+// Space Complexity: O(n)
+const sweetAndSavory = (dishes, target) => {
+  const sweetDishes = dishes.filter(dish => dish < 0).sort((a,b) => b-a);
+  const savoryDishes = dishes.filter(dish => dish > 0).sort((a,b) => a-b);
+
+  let bestPair = [0,0];
+  let bestDiff = Infinity;
+
+  let sweetIdx = 0, 
+   savoryIdx = 0;
+
+  while(sweetIdx < sweetDishes.length && savoryIdx < savoryDishes.length) {
+    const currSum = savoryDishes[savoryIdx] + sweetDishes[sweetIdx];
+
+    if(currSum <= target) {
+      const currDiff = target - currSum;
+
+      if(currDiff < bestDiff) {
+        bestDiff = currDiff;
+        bestPair = [sweetDishes[sweetIdx], savoryDishes[savoryIdx]];
+      }
+
+      savoryIdx++;
+    } else {
+      sweetIdx++;
+    }
+
+
+  }
+
+  return bestPair;
+
 }
 
-
-const dishes = [-5,-3,1,7];
-const target = 8;
-
+const dishes = [-3,-5, 1,7];
+const target = 8; // [-1, 7] 6
 
 console.log(sweetAndSavory(dishes, target));
 
