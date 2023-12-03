0e04bb3ab0590095cc4a3b805937dc83f10ca2c6
mambatukaa <sbatuka9@gmail.com>
2023-12-02 23:28:04 -0800

    lowest common ancestor

---
+++ dsaLeetCode/treesAndGraph/lowestCommonAncestorBT.js
@@ -0,0 +1,16 @@
+const lowestCommonAncestor = (root) => {
+
+}
+
+
+class BT {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const root = new BT(1);
+
+lowestCommonAncestor(root);
