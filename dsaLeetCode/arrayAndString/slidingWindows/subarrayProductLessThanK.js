fd19a79ddcdd6173bcfca9d76cefd242838a42b2
mambatukaa <sbatuka9@gmail.com>
2023-12-15 21:15:08 -0800

    add solution using sliding window

---
+++ dsaLeetCode/arrayAndString/slidingWindows/subarrayProductLessThanK.js
@@ -0,0 +1,64 @@
+// brute force
+const arrayProduct = (nums, k) => {
+  let counter = left = 0;
+  let sum = 1;
+
+  while(left < nums.length) {
+    let right = left;
+
+    if(nums[left] < k) {
+      counter++;
+    };
+
+    sum *= nums[left];
+
+    while(right < nums.length) {
+      right++;
+
+      sum *= nums[right];
+
+      if(sum < k) {
+        counter++;
+      }     
+
+    };
+
+    sum = 1;
+    left++;
+  };
+
+  return counter;
+};
+
+const arrayProductSum = (nums, k) => {
+  if(k <= 1) {
+    return 0;
+  };
+
+  let left = 0;
+  let curr = 1;
+  let answer = 0;
+
+  for(let right = 0; right < nums.length; right++) {
+    // slide
+    curr = curr * nums[right];
+
+    while(curr >= k) {
+      //shrink
+      curr = curr / nums[left];
+      left++;
+    };
+
+    console.log(curr,'hahhaha')
+
+    answer += right - left + 1;
+  };
+
+  return answer;
+};
+
+
+const k = 1;
+const nums = [10, 5, 2, 6];
+
+console.log(arrayProductSum(nums, k));
