7f32d6ed77bf206718429481696af254bc42e4d5
mambatukaa <sbatuka9@gmail.com>
2024-01-04 11:13:58 -0800

    add solution for unique numbers x+1, x-1 in nums

---
+++ dsaLeetCode/hashing/uniqueNumbersInX.js
@@ -0,0 +1,17 @@
+const fn = (nums) => {
+  const numsSet = new Set(nums);
+  const ans = [];
+  
+  for(const num of nums) {
+    if(!numsSet.has(num + 1) && !numsSet.has(num - 1)) {
+      ans.push(num);
+    };
+  };
+
+  return ans;
+};
+
+
+const nums = [1, 3, 5, 6, 8]
+console.log(fn(nums));
+// answer = [1, 3, 8];
