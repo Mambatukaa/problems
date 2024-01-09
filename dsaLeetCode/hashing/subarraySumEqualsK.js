7945c3c08ed09ce24809f2bc1cd018cb7e8503d8
mambatukaa <sbatuka9@gmail.com>
2024-01-08 23:13:32 -0800

    add solution using hashMap and prefix

--- dsaLeetCode/hashing/subarraySumEqualsK.js
+++ dsaLeetCode/hashing/subarraySumEqualsK.js
@@ -4,43 +4,41 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let counter = 0;
-    let answer = 0;
-    let l = 0;
+  const map = new Map();
+  map.set(0, 1);
+  
+  let curr = 0;
+  let ans = 0;
 
-    for(let r = 0; r < nums.length; r++) {
-        counter += nums[r];
+  for(const num of nums) {
+    curr += num;
+    ans += map.get(curr - k) || 0;
 
-        while(counter >= k && counter !== 0) {
-            if(counter === k) {
-                // update the answer
-                answer++
-            };
+    map.set(curr, (map.get(curr) || 0) + 1);
+  };
 
-            counter -= nums[l];
-            l++;
-        };
-    }
+  console.log(map)
 
-    return answer;
+  return ans;
 };
 
 
 /*
-
-nums = [1,2,3,4,5] k = 6 => [1,2,3].lenght = 3
+nums = [1,2,1,2,1] k = 3 =>  [1,2], [2,1], [2,1] total subarrays equal 3;
 output: 3
 
-/ *
-k = 6;
+k = 3;
+nums:
+[1, 2, 1, 2, 1]
 
-       l
-       r
-[1,2,3,4,5];
+prefix:
+[1, 3, 4, 6, 7];
 
-counter = 1 + 2 = 3 + 3 = 6 => update answer => shrink => 5 => 9 > k => shrink ;
-answer = 0 => 3 =>; 
+
+[ -1, -1, 1 ] k = 0
+[ -1, -2, -1 ]
 */
-// check counter === k update answer, 
-// check counter > k shrink the substring
 
+const nums = [-1, -1, 1]
+
+console.log(subarraySum(nums, 0));
