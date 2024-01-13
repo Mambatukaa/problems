8edb7cfaa43d8946c8822b1734e5f51f65bd154a
mambatukaa <sbatuka9@gmail.com>
2024-01-13 11:13:27 -0800

    add solution using additional array

--- dsaLeetCode/hashing/contigiuousArray.js
+++ dsaLeetCode/hashing/contigiuousArray.js
@@ -24,14 +24,37 @@ const fn = (nums) => {
 
   return max;
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const fnII = (nums) => {
+  const arr = new Array(nums.length * 2 + 1).fill(-2);
+
+  arr[nums.length] = -1;
+
+  let maxLength = 0;
+  let count = 0;
+  const n = nums.length;
+
+  for(let i = 0; i < n; i++) {
+    count = count + nums[i] === 0 ? -1 : 1;
+
+    if(arr[count + n] >= -1) {
+      maxLength = Math.max(maxLength, i - arr[count + n]);
+    } else {
+      arr[count + n] = i;
+    };
+
+  };
+
+  console.log(arr, 'hahaahah')
+
+  return maxLength;
+};
+
 const nums = [0, 1, 1, 0, 1, 1, 1, 0];
 
-console.log(fn(nums));
-
-/*
-
- */
-
+console.log(fnII(nums));
 
 
 
