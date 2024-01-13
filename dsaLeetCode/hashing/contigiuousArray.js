f5051f64be55dbe34226374b9de4936805cd57a1
mambatukaa <sbatuka9@gmail.com>
2024-01-13 11:07:55 -0800

    add solution using map

--- dsaLeetCode/hashing/contigiuousArray.js
+++ dsaLeetCode/hashing/contigiuousArray.js
@@ -1,10 +1,30 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 const fn = (nums) => {
+  const map = new Map();
+
+  // initial value
+  map.set(0, -1);
+
+  let max = 0;
+  let counter = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    nums[i] === 1 ? counter++ : counter--; 
+
+    if(map.has(counter)) {
+      max = Math.max(max, i - map.get(counter));
+    } else {
+        map.set(counter, i);
+    };
+
+  };
+
+  console.log(map)
+
+  return max;
 }
-      //      L                                                                                                                                                                                                     R
-const nums = [0,1,0,1,1,1,0,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,1,0,1,0,0,1,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,0,1,1,0,0,0,1,1];
-console.log(nums.length)
-// 0:3, 1:5
-// 
+const nums = [0, 1, 1, 0, 1, 1, 1, 0];
 
 console.log(fn(nums));
 
