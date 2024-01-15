8082ee63c660687e13f9a5de308bb3d4ae5f33c8
mambatukaa <sbatuka9@gmail.com>
2024-01-14 22:30:10 -0800

    add solution using ascii code

--- dsaLeetCode/hashing/groupAnagrams.js
+++ dsaLeetCode/hashing/groupAnagrams.js
@@ -28,7 +28,35 @@ var groupAnagrams = function(strs) {
     return answer;
 };
 
+// Time Complexity: O(n * m);
+// Space Complexity: O(n * m)
+const groupAnagramsII = (strs) => {
+  const map = new Map();
+
+  for(const str of strs) {
+    let counter = 0;
+
+    for(const letter of str) {
+      counter+= letter.charCodeAt(0);
+    };
+
+    if(!map.has(counter)) {
+      map.set(counter, []);
+    };
+
+    map.get(counter).push(str);
+
+  };
+
+  const answer = [];
+
+  for(const value of map.values()) {
+    answer.push(value);
+  };
+
+  return answer; 
+};
+
 const strs = ["eat", "tea", "tan", "nat", "bat"];
 
-console.log(groupAnagrams(strs));
-
+console.log(groupAnagramsII(strs));
