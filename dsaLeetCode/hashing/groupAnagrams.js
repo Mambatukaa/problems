242dbf461040b3c1ca63ef1cb7bf39bb6d681fa2
mambatukaa <sbatuka9@gmail.com>
2024-01-14 19:33:21 -0800

    update

--- dsaLeetCode/hashing/groupAnagrams.js
+++ dsaLeetCode/hashing/groupAnagrams.js
@@ -4,34 +4,26 @@
  */
 
 // Naive solution
-// Time Complexity: O(n * n logn)
-// Space Complexity: O(n)
+// Time Complexity: O(n * m logm) // m = string
+// Space Complexity: O(n * m)
 var groupAnagrams = function(strs) {
     const map = new Map();
-    let idx = 0;
 
     for(const str of strs) {
         const sortedStr = str.split("").sort().join("") ;
 
         if(!map.has(sortedStr)) {
-            map.set(sortedStr, idx);
-            idx++;
+            map.set(sortedStr, []);
         };
+
+        map.get(sortedStr).push(str);
     };
 
     const answer = [];
 
-    for(const str of strs) {
-        const sortedStr = str.split("").sort().join("") ;
-        const key = map.get(sortedStr);
-
-        if(!answer[key]) {
-            answer[key] = [str];
-        } else {
-            answer[key].push(str);
-        }
-
-    };
+    for(const group of map.values()) {
+      answer.push(group);
+    }
 
     return answer;
 };
