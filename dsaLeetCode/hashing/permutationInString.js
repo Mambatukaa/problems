b6c7da32c1119f154ccf803fab4fa2208837a737
mambatukaa <sbatuka9@gmail.com>
2024-01-23 14:42:56 -0800

    update

--- dsaLeetCode/hashing/permutationInString.js
+++ dsaLeetCode/hashing/permutationInString.js
@@ -4,64 +4,6 @@
  * @return {boolean}
  */
 
-// brute force
-// TC: O(n)
-// SC: O(n)
-// Time limit exceeded
-var checkInclusion = function(s1, s2) {
-    const map = new Map();
-
-    for(const ch of s1) {
-        map.set(ch, (map.get(ch) || 0) + 1);
-    };
-
-    console.log(map)
-
-    for(let i = 0; i < s2.length; i++) {
-        const newMap = new Map(map);
-
-        if(!newMap.has(s2[i])) {
-            continue;
-        };
-
-        newMap.set(s2[i], map.get(s2[i]) - 1);
-
-        if(newMap.get(s2[i]) === 0) {
-            newMap.delete(s2[i]);
-        };
-
-        
-        if(newMap.size === 0) {
-            return true;
-        }
-
-        for(let j = i + 1; j < s2.length; j++) {
-            if(!newMap.has(s2[j])) {
-                console.log('----------------------')
-                break;
-            };
-
-        newMap.set(s2[j], newMap.get(s2[j]) - 1);
-
-        if(newMap.get(s2[j]) === 0) {
-            newMap.delete(s2[j]);
-        };
-
-        console.log(newMap, '------', s2[j])
-
-        if(newMap.size === 0) {
-            return true;
-        }
-
-
-        };
-
-
-    };
-
-    return false;
-    
-};
 
 // sorting
 // Time Limit Exceeded
@@ -76,9 +18,105 @@ const checkInclusionII = (s1, s2) => {
     };
   };
 
-  return false;};
+  return false;
+};
+
+
+
+// HashMaps
+// Time limit Exceeded
+// l1 be the length of string s1
+// l2 be the length of string s1
+// Time Complexity: O(l1 + 26l1(l2 - l1))
+// Space Complexity: O(1) Hashmap contains at most 26 key value pairs
+
+var checkInclusion = function(s1, s2) {
+  if(s1.length > s2.length) {
+    return false;
+  };
+
+  const s1Map = new Map();
+
+  for(const ch of s1) {
+    s1Map.set(ch, (s1Map.get(ch) || 0) + 1);
+  };
+
+
+  for(let i = 0; i < s2.length; i++) {
+    const s2Map = new Map();
+
+    for(let j = i; j < s1.length + i; j++) {
+      s2Map.set(s2[j], (s2Map.get(s2[j]) || 0) + 1);
+    };
+
+    if(matches(s1Map, s2Map)) {
+      return true;
+    }
+
+  };
+
+  return false;
+    
+};
+
+const matches = (map1, map2) => {
+  for(const [key, value] of map1) {
+    if(map2.get(key) !== value) {
+      return false;
+    };
+  };
+
+  return true;
+};
+
+// Time limit exceeded
+// s1 s2 is lower case english letters
+// l1 be the length of string s1
+// l2 be the length of string s1
+// Time Complexity: O(l1 + 26l1(l2 - l1))
+// Space Complexity: O(1) Hashmap contains at most 26 key value pairs
+const checkInclusionIII = (s1, s2) => {
+  if(s1.length > s2.length) {
+    return false;
+  }
+
+  const s1Arr = new Array(26).fill(0);
+
+  for(let i = 0; i < s1.length; i++) {
+    s1Arr[s1.charCodeAt(i) - "a".charCodeAt(0)]++;
+  };
+
+  console.log(s1Arr)
+
+  for(let i = 0; i < s2.length; i++) {
+    const s2Arr = new Array(26).fill(0);
+
+    for(let j = i; j < i + s1.length; j++) {
+     s2Arr[s2.charCodeAt(j) - "a".charCodeAt(0)]++;
+    };
+
+    if(arrMatches(s1Arr, s2Arr)) {
+      return true;
+    };
+
+  };
+
+  return false;
+};
+
+const arrMatches = (arr1, arr2) => {
+  for(let i = 0; i < 26; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
 
 const s1 = "ba";
 const s2 = "eidbaooo";
 
-console.log(checkInclusionII(s1, s2));
+console.log(checkInclusionIII(s1, s2));
