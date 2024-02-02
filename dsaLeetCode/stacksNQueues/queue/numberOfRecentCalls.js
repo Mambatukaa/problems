7e647ca880ec888a6a1ea5f65d4dfc0ebf3aff81
mambatukaa <sbatuka9@gmail.com>
2024-02-02 11:26:27 -0800

    add solution using queue

---
+++ dsaLeetCode/stacksNQueues/queue/numberOfRecentCalls.js
@@ -0,0 +1,44 @@
+// Space Complexity: O(n)
+var RecentCounter = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+RecentCounter.prototype.ping = function(t) {
+  this.queue.push(t);
+
+  while(this.queue[0] < t - 3000) {
+    this.queue.shift();
+  }; 
+
+  return this.queue.length;
+};
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
+
+
+ /*
+  [1, 100, 3001, 3002]
+
+
+  newRequest
+  oldRequest = queue[0];
+
+
+  while(oldRequest < newRequest - 3000) {
+    oldRequest = queue.shift();
+  }
+
+  return queue.length;
+
+
+ */
