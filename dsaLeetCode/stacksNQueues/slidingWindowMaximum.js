4596e227644062b0e9821ce4ece2f32ab6c1e64b
mambatukaa <sbatuka9@gmail.com>
2024-02-06 09:20:31 -0800

    add optimal solution

--- dsaLeetCode/stacksNQueues/slidingWindowMaximum.js
+++ dsaLeetCode/stacksNQueues/slidingWindowMaximum.js
@@ -27,8 +27,46 @@ const fn = (nums, k) => {
 };
 
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+// Time Complexity: O(n)
+// Space Complexity: O(k)
+var maxSlidingWindow = function(nums, k) {
+  const queue = [];
+  const answer = [];
+
+  for(let i = 0; i < nums.length; i++) {
+
+    // update queue by max element
+    while(queue.length && nums[queue[queue.length - 1]] < nums[i]) {
+      queue.pop();
+    };
+
+    queue.push(i);
+
+    // remove old element
+    if(queue[0] + k === i) {
+      queue.shift();
+    };
+
+    // update an answer 
+    if(i >= k - 1) {
+      answer.push(nums[queue[0]]);
+    };
+  };
+
+
+  return answer;
+ 
+};
+
 
 /*
+ 
+ 1 => [0] => 3 => [0, 1] => -1 => [2, 0, 1] => ans = [3] => -3 => 
 
 
   nums = [1, 3, -1, -3, 5, 3, 6, 7]; k = 3;
@@ -44,6 +82,20 @@ const fn = (nums, k) => {
    1  3  -1 [-3  5  3] 6  7      5
    1  3  -1  -3 [5  3  6] 7      6
    1  3  -1  -3  5 [3  6  7]     7
+
+
+   1. Brute force (TIME LIMIT EXCEEDED)
+
+   2. Double ended queue (deque);
+      a. Declare queue which store values index's by order.
+      b. Declare answer which stores answer.
+      c. iterate through nums
+      d. while(current element) is greater than the queue's last element pop() element from queue
+      e. add element to the queue
+      f. remove old element
+      g. add element to the answer queue[0]
+      return answer;
+
  */
 
 const nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3;
