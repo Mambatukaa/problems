68c9fba8c8358734f18249f52378f5fdfd9abee4
mambatukaa <sbatuka9@gmail.com>
2024-02-12 09:57:53 -0800

    add solution using recursive and iterative

---
+++ dsaLeetCode/treesAndGraph/maxAncestorDiff.js
@@ -0,0 +1,154 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  };
+};
+
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  };
+
+
+  insert(val) {
+    const newNode = new TreeNode(val);
+
+    if(!this.root) {
+      this.root = newNode;
+    } else {
+      const queue = [this.root];
+
+      while(queue.length) {
+        const curr = queue.shift();
+
+        if(!curr.left) {
+          curr.left = newNode;
+          return;
+        };
+
+        if(!curr.right) {
+          curr.right = newNode;
+          return;
+        };
+
+        queue.push(curr.left);
+        queue.push(curr.right);
+
+      };
+
+    };
+  }
+};
+
+const root = new TreeNode(8);
+
+const node2 = new TreeNode(3);
+const node3 = new TreeNode(10);
+const node4 = new TreeNode(1);
+const node5 = new TreeNode(6);
+
+const node6 = new TreeNode(14);
+const node7 = new TreeNode(4);
+const node8 = new TreeNode(7);
+const node9 = new TreeNode(13);
+
+root.left = node2;
+root.right = node3;
+
+node2.left = node4;
+node2.right = node5;
+
+node3.right = node6;
+
+node5.left = node7;
+node5.right = node8;
+
+node6.left = node9;
+
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const maxAncestorDiff = (root) => {
+  if(!root) {
+    return null;
+  };
+
+  const helper = (node, curMax, curMin) => {
+    if(!node) {
+      // find diff
+      return curMax - curMin;
+    };
+
+    curMax = Math.max(curMax, node.val);
+    curMin = Math.min(curMin, node.val);
+
+    const left = helper(node.left, curMax, curMin);
+    const right = helper(node.right, curMax, curMin);
+
+    return Math.max(left, right);
+  };
+
+
+  return helper(root, root.val, root.val);
+};
+
+// Space Complexity: O(n)
+// Time Complexity: O(n)
+const maxAncestorDiffII = (root) => {
+  if(!root) {
+    return null;
+  };
+
+  let answer = -Infinity;
+  let max = root.val;
+  let min = root.val;
+  const stack = [];
+
+  stack.push([root, max, min]);
+
+  while(stack.length) {
+    let [node, max, min] = stack.pop();
+
+
+    max = Math.max(node.val, max);
+    min = Math.min(node.val, min);
+
+    const diff = max - min;
+
+    if(diff > answer) {
+      answer = diff;
+    };
+
+    if(node.left) {
+      stack.push([node.left, max, min]);
+    };
+
+    if(node.right) {
+      stack.push([node.right, max, min]);
+    };
+  };
+
+  return answer;
+};
+
+console.log(maxAncestorDiffII(root));
+
+/*
+ 
+                   8
+                 /   \
+                3     10
+               / \      \
+              1   6      14
+                 / \     /
+                4   7   13
+
+1. Declare stack, min max.
+2. Get node from stack and update min, max;
+3. When get min, max from stack update answer using the diff of max and min.
+4. Pass min, max to the children
+ */
+
