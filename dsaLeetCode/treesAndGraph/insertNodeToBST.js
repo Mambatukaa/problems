135e5df2826af5994cb9482263a30fa4ad7eea94
mambatukaa <sbatuka9@gmail.com>
2024-02-17 22:30:35 -0800

    add bst insertion using both iterative and recursion

---
+++ dsaLeetCode/treesAndGraph/insertNodeToBST.js
@@ -0,0 +1,94 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+var insertIntoBSTII = function(root, val) {
+  const newNode = new TreeNode(val);
+
+  if(!root) {
+    return newNode;
+  };
+
+
+  if(root.val < val) {
+    // go right
+
+    if(!root.right) {
+      root.right = newNode;
+      return root;
+    }
+
+    insertIntoBST(root.right, val);
+
+  } else {
+    if(!root.left) {
+      root.left = newNode;
+      return root;
+    }
+
+    insertIntoBST(root.left, val);
+  }
+
+  return root;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const insertIntoBST = (root, val) => {
+  const nodeToInsert = new TreeNode(val);
+
+  if(!root) {
+    return nodeToInsert;
+  }
+
+  const stack = [root];
+
+  while(stack.length) {
+    let curr = stack.pop();
+
+    if(curr.val > val) {
+      // go left
+      if(!curr.left) {
+        curr.left = nodeToInsert;
+        break;
+      };
+
+      stack.push(curr.left);
+
+    } else {
+      // go right
+      if(!curr.right) {
+        curr.right = nodeToInsert;
+        break;
+      }
+
+      stack.push(curr.right);
+    };
+
+  };
+  return root;
+};
+
+
+/*
+
+
+          4
+        /   \
+      2       7
+    /  \     /
+  1     3   5
+
+
+*/
