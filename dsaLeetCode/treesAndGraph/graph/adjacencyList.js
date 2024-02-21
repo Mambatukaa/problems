acf1eaf05df01c173daba2f00020c44b928277e2
mambatukaa <sbatuka9@gmail.com>
2024-02-20 18:55:27 -0800

    update

--- dsaLeetCode/treesAndGraph/graph/adjacencyList.js
+++ dsaLeetCode/treesAndGraph/graph/adjacencyList.js
@@ -21,6 +21,13 @@ class Graph {
     second.neighbors.push(first);
   };
 
+  addDirectedEdge(i, j) {
+    const first = this.nodeList[i];
+    const second = this.nodeList[j];
+
+    first.neighbors.push(second)
+  };
+
   toString() {
     let string = "";
 
@@ -111,6 +118,53 @@ class Graph {
       vertice.isVisited = false;
     };
   };
+
+
+  // Time Complexity: O(E)
+  // Space Complexity: O(E)
+  topologicalSortHelper(vertice, stack) {
+    // if vertex depends on the currentVertex
+      // Go to that vertex
+      // then come back to currentVertex
+    // else
+      // Push currentVertex to Stack
+
+    const neighbors = vertice.neighbors;
+
+    if(neighbors.length) {
+      // TC: O(m), m = neighbors
+      for(const neighbor of neighbors) {
+        if(neighbor.visited) continue;
+        // TC: O(m)
+        this.topologicalSortHelper(neighbor, stack);
+        neighbor.visited = true;
+      };
+    } 
+
+    stack.push(vertice);
+
+    vertice.visited = true;
+  };
+
+  // Time Complexity: O(V + E)
+  // Space Complexity: O(V + E)
+  topologicalSort() {
+    const stack = [];
+    // Time Complexity: O(V)
+    // Space Complexity: O(V)
+    for(const vertice of this.nodeList) {
+      if(!vertice.visited)
+      // Time Complexity: O(E)
+      // Space Complexity: O(E)
+        this.topologicalSortHelper(vertice, stack);
+    };
+
+    while(stack.length) {
+      const curr = stack.pop();
+
+      console.log(curr.name);
+    }
+  };
   
 };
 
@@ -148,4 +202,39 @@ graph.dfs()
  C: -> A -> D
  D: -> A -> C -> E
  E: -> B -> D
+
+
+
+      A       B
+        \   /  \
+          C     D
+         /     /
+        E     /
+      /  \   /
+     H     F ---> G
+
+
+A: -> C
+B: -> C -> D
+C: -> E
+D: -> F
+E: -> F -> H
+F: -> G
+G:
+H:
+
+A->B->C->D->E->H->F->G
+
+
+  A B C D E F G H
+A 0 0 1 0 0 0 0 0
+B 0 0 1 1 0 0 0 0
+C 0 0 0 0 1 0 0 0
+D 0 0 0 0 0 1 0 0
+E 0 0 0 0 0 1 0 1
+F 0 0 0 0 0 0 1 0
+G 0 0 0 0 0 0 0 0
+H 0 0 0 0 0 0 0 0
+
+
  */
