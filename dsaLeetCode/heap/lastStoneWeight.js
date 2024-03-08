36f3bcd7197ebc1c830f915f30e9d830b5f533b7
mambatukaa <sbatuka9@gmail.com>
2024-03-08 10:48:27 -0800

    add solution that uses heapify method

--- dsaLeetCode/heap/lastStoneWeight.js
+++ dsaLeetCode/heap/lastStoneWeight.js
@@ -27,6 +27,68 @@ var lastStoneWeight = function(stones) {
 };
 
 
+function heapifyHelper(arr, index) {
+  const n = arr.length;
+  const leftChildIdx = index * 2 + 1;
+  const rightChildIdx = index * 2 + 2;
+  let largestIdx = index;
+
+  if(leftChildIdx < n && arr[leftChildIdx] > arr[largestIdx]) {
+    largestIdx = leftChildIdx;
+  };
+
+  if(rightChildIdx < n && arr[rightChildIdx] > arr[largestIdx]) {
+    largestIdx = rightChildIdx;
+  };
+
+  if(index !== largestIdx) {
+    //swap
+    [arr[index], arr[largestIdx]] = [arr[largestIdx], arr[index]];
+    
+    heapifyHelper(arr, largestIdx);
+  };
+};
+
+function heapify(arr) {
+  for(let i = Math.floor(arr.length / 2) - 1; i >= 0; i--) {
+    heapifyHelper(arr, i);
+  }
+}
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeightII = function(stones) {
+  while(stones.length > 1) {
+    heapify(stones);
+
+    console.log(stones)
+
+    const y = stones[0];
+
+    if(stones[1] < stones[2]) [stones[2], stones[1]] = [stones[1], stones[2]];
+
+    const x = stones[1];
+
+    console.log(x, '====', y)
+
+    if(y === x) {
+      stones.shift();
+      stones.shift();
+    } else {
+      stones.shift();
+      stones[0] = y - x;
+    };
+
+  };
+
+  return stones[0] || 0;
+
+
+};
+
+
 /*
 
 1. get 2 [x,y] max stone from stones
