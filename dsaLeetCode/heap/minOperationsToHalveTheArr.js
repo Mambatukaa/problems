a905cc6564e9fd596aea77683c22873393b4a403
mambatukaa <sbatuka9@gmail.com>
2024-03-09 07:54:01 -0800

    add naive solution

---
+++ dsaLeetCode/heap/minOperationsToHalveTheArr.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ // Time Complexity: O(n * n log n)
+ // Space Complexity: O(1)
+var halveArray = function(nums) {
+  let total = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+  const expectedValue = total / 2;
+
+  let counter = 0;
+
+  while(expectedValue < total) {
+    nums.sort((a, b) => b - a);
+
+    nums[0] = nums[0] / 2;
+    total -= nums[0];
+
+    counter++;
+  };
+
+
+  return counter;
+    
+};
+
+
+/*
+
+5, 19, 8, 1 ===== total = 33;
+reduce to 33 / 2 = 16.5 > reduced
+
+
+3,8,20 ===== total = 31
+
+reduce to 31 / 2 = 15.5 > reduced
+
+
+1. divide max value by 2 and update answer.
+      if answer less than expected value return total operations
+   do the same operation until reach the expected value
+   update the array each time divide the max value
+
+
+
+
+*/
