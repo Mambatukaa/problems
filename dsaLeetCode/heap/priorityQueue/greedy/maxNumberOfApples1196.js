bb8cf8aa2f293196afedfd9df448dd14f14e2457
mambatukaa <sbatuka9@gmail.com>
2024-03-19 10:06:54 -0700

    add solutionN

--- dsaLeetCode/heap/priorityQueue/greedy/maxNumberOfApples1196.js
+++ dsaLeetCode/heap/priorityQueue/greedy/maxNumberOfApples1196.js
@@ -2,8 +2,9 @@
  * @param {number[]} weight
  * @return {number}
  */
+// Sort also we can use min Heap and Counting sort
 // Time Complexity: O(n log n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
 var maxNumberOfApples = function(weight) {
   weight.sort((a, b) => a - b);
 
