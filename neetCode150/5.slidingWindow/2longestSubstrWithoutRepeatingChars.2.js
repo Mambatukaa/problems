85bf4132e2c2f346883fdc8c4d6748d7b5c3f91b
mambatukaa <sbatuka9@gmail.com>
2024-04-18 14:13:25 -0700

    add solution using sliding window

---
+++ neetCode150/5.slidingWindow/2longestSubstrWithoutRepeatingChars.2.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+
+// 5 minutes
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+var lengthOfLongestSubstring = function(s) {
+  // iteration will be faster
+  const arr = s.split("");
+  const n = arr.length;
+  let res = 0;
+  let left = 0;
+
+  // check repetition;
+  const seen = new Set();
+
+  for(let right = 0; right < n; right++) {
+
+    while(seen.has(arr[right])) {
+      seen.delete(arr[left]);
+
+      left++;
+    };
+
+    seen.add(arr[right]);
+
+    res = Math.max(res, right - left + 1);
+  };
+
+  return res;
+};
+
+
+
+/*
+
+longest substring without repeating characters
+
+
+
+
+
+
+*/
