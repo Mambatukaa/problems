5875dddf37ecba38bbe4e54cd243e78bc763e626
mambatukaa <sbatuka9@gmail.com>
2024-04-19 21:52:00 -0700

    add solution using sliding window

---
+++ neetCode150/5.slidingWindow/3longestRepeatingCharacterReplacement.424.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+      n = len(s)
+
+      answer = 0
+      count = {}
+      left = 0
+
+      for right in range(n):
+        count[s[right]] = count.get(s[right], 0) + 1 
+
+        # is invalid SHRINK THE WINDOW
+        # n - count[max] > k
+        while (right - left + 1) - max(count.values()) > k:
+          count[s[left]] -= 1
+          left += 1
+
+        # update answer
+        answer = max(answer, right - left + 1)
+
+      return answer
+
