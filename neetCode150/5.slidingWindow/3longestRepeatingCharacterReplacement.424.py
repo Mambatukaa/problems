83a3a4a03302ff33133dd293c47f9c2cd612d442
mambatukaa <sbatuka9@gmail.com>
2024-04-19 22:20:58 -0700

    update

--- neetCode150/5.slidingWindow/3longestRepeatingCharacterReplacement.424.py
+++ neetCode150/5.slidingWindow/3longestRepeatingCharacterReplacement.424.py
@@ -13,7 +13,7 @@ class Solution:
         count[s[right]] = count.get(s[right], 0) + 1 
 
         # is invalid SHRINK THE WINDOW
-        # n - count[max] > k
+        # window size - count[max] > k
         while (right - left + 1) - max(count.values()) > k:
           count[s[left]] -= 1
           left += 1
