32ad2ed671dd0b3c267b93191ebbd115197af389
mambatukaa <sbatuka9@gmail.com>
2024-04-26 10:43:25 -0700

    update

--- neetCode150/7.trees/1.invertBinaryTree.226.py
+++ neetCode150/7.trees/1.invertBinaryTree.226.py
@@ -41,3 +41,49 @@ class Solution1:
       return root
 
 
+# BFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+
+        queue = collections.deque([root])
+
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
+
+        return root
+        """
+        if root is None:
+            return root
+
+        else:
+
+            #temp = root.left
+            #root.left = self.invertTree(root.right)
+            #root.right = self.invertTree(temp)
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root 
+        """
+        """
+        if not root:
+            return root
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root        
+        """
