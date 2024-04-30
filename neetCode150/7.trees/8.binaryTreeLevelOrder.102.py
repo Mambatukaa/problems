bc624e83eabc2696ab3503b51cd9c00ceb20d9e6
mambatukaa <sbatuka9@gmail.com>
2024-04-30 11:15:38 -0700

    add recursive solution

--- neetCode150/7.trees/8.binaryTreeLevelOrder.102.py
+++ neetCode150/7.trees/8.binaryTreeLevelOrder.102.py
@@ -36,4 +36,43 @@ class Solution:
         res.append(subRes)
 
       return res
+
+
+
+  # ***********************8
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# 5 minutes
+# Recursive
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+      res = []
+
+      def dfs(root, level):
+        if not root:
+          return
+        
+        if len(res) == level:
+          res.append([])
+        
+        res[level].append(root.val)
+
+        if root.left:
+          dfs(root.left, level + 1)
+        if root.right:
+          dfs(root.right, level + 1)
+
+      dfs(root, 0)
+
+      return res
+
+
+
