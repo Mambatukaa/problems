4799fd71c3326d9c96c12e930cf04176dae05296
mambatukaa <sbatuka9@gmail.com>
2024-04-30 14:04:07 -0700

    add solution using iteration

--- neetCode150/7.trees/10.countGoodNodesInBT.1448.py
+++ neetCode150/7.trees/10.countGoodNodesInBT.1448.py
@@ -34,3 +34,33 @@ class Solution:
 
 
     """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# 10 minutes
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+      counter = 0
+
+      stack = [[root, root.val]]
+
+      while len(stack):
+        [curr, largest] = stack.pop()
+
+        if curr.val >= largest:
+          counter += 1
+          largest = curr.val
+
+        if curr.left:
+          stack.append([curr.left, largest])
+        if curr.right:
+          stack.append([curr.right, largest])
+
+      return counter
+
