471912eac955647513ce5000376da4a93fcde0e5
mambatukaa <sbatuka9@gmail.com>
2024-04-30 22:35:47 -0700

    add solution

--- neetCode150/7.trees/12.kthSmallestElelementInaBST.230.py
+++ neetCode150/7.trees/12.kthSmallestElelementInaBST.230.py
@@ -27,4 +27,40 @@ class Solution:
         counter += 1
         curr = curr.right
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Recursion
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        def dfs(root, k):
+            if not root:
+                return k, -1
+            
+            k, val = dfs(root.left, k)
+
+            if val >= 0:
+              return k, val
+
+            k -= 1
+
+            if k == 0:
+              return k, root.val
+            
+            return dfs(root.right, k)
+
+        _, val = dfs(root, k)
+
+        return val
+
+
 # In order traversal and return k th element
+
+
+
