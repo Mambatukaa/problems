45552feab893891e29839a4d8d164c33dd166286
mambatukaa <sbatuka9@gmail.com>
2024-04-30 21:12:17 -0700

    add iterative BST

--- neetCode150/7.trees/11.validateBinarySearchTree.98.py
+++ neetCode150/7.trees/11.validateBinarySearchTree.98.py
@@ -7,6 +7,7 @@
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 # 1 hour
+# Recursive
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
       def dfs(root, minRange, maxRange):
@@ -19,5 +20,33 @@ class Solution:
         return dfs(root.left, minRange, root.val) and dfs(root.right, root.val, maxRange)
       
       return dfs(root, -float('inf'), float('inf'))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# 1 hour
+# Iterative
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+      queue = deque([[root, -float('inf'), float('inf')]])
+
+      while len(queue):
+        [curr, minRange, maxRange] = queue.popleft()
+
+        if curr.val <= minRange or curr.val >= maxRange:
+          return False
+        
+        if curr.left:
+          queue.append([curr.left, minRange, curr.val])
+        if curr.right:
+          queue.append([curr.right, curr.val, maxRange])
+      
+
+      return True
 
 
