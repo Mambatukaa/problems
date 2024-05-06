334e1e675bb411b8c6d7bb3c91d7ad5814f73150
mambatukaa <sbatuka9@gmail.com>
2024-05-06 11:12:55 -0700

    add solution using dfs

--- neetCode150/9.backtracking/2.combinationSum.39.py
+++ neetCode150/9.backtracking/2.combinationSum.39.py
@@ -1,22 +1,27 @@
+# Time Complexity: O(2^t)
+# Space Complexity: O(2^t)
+# Gave up
 class Solution:
     def combinationSum(self, candidates, target):
       res = []
 
-      def backtracking(curr, currSum):
-        if currSum > target:
+      def backtracking(curr, currSum, idx):
+        if idx >= len(candidates) or currSum > target:
           return
 
         if currSum == target:
           res.append(curr.copy())
+          return
+
         
-        for i in range(len(candidates)):
+        for i in range(idx, len(candidates)):
           candidate = candidates[i]
           curr.append(candidate)
-          backtracking(curr, currSum + candidate)
+          backtracking(curr, currSum + candidate, idx)
           curr.pop()
-        print('---------', curr)
+          idx += 1
 
-      backtracking([], 0)
+      backtracking([], 0, 0)
         
       return res
 
@@ -24,3 +29,32 @@ class Solution:
 solution = Solution()
 
 print(solution.combinationSum([2,3,6,7], 7))
+
+# Neetcode solution 
+# Decision tree
+# 2 decisions one with current one with next element
+class Solution1:
+    def combinationSum(self, candidates, target):
+      res = []
+
+      def backtracking(curr, currSum, idx):
+        if idx >= len(candidates) or currSum > target:
+          return
+
+        if currSum == target:
+          res.append(curr.copy())
+          return
+
+        curr.append(candidates[idx])
+
+        backtracking(curr, currSum + candidates[idx], idx)
+        curr.pop()
+        backtracking(curr, currSum, idx + 1)
+
+      backtracking([], 0, 0)
+        
+      return res
+
+solution1 = Solution1()
+
+print(solution1.combinationSum([2,3,6,7], 7))
