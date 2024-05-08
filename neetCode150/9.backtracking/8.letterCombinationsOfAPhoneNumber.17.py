f9bfe9f415ffedccd134c8664b4438c583a1fac2
mambatukaa <sbatuka9@gmail.com>
2024-05-07 17:21:27 -0700

    add solution using backtracking

---
+++ neetCode150/9.backtracking/8.letterCombinationsOfAPhoneNumber.17.py
@@ -0,0 +1,33 @@
+# Time Complexity: O(n * 2^n) n = digits.length
+# Space Complexity: O(n * 2^n)
+# 20 minutes
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+      digitsMap = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+      }
+
+      res = []
+
+      def backtracking(curr, start):
+        if len(curr) == len(digits):
+          res.append(curr)
+          return
+
+        for letter in digitsMap[digits[start]]:
+          curr += letter
+          backtracking(curr, start + 1)
+          curr = curr[:-1]
+      
+      if digits:
+        backtracking("", 0)
+      
+      return res
+        
