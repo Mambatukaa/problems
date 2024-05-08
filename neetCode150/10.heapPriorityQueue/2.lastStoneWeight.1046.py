909a5825a2f74161433b9c2a67ab48bc767e869c
mambatukaa <sbatuka9@gmail.com>
2024-05-07 19:33:54 -0700

    add solution using max heap

---
+++ neetCode150/10.heapPriorityQueue/2.lastStoneWeight.1046.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop, heapify
+
+# Time Complexity: O(log n)
+# Space Complexity: O(log n)
+# 10 minutes
+# Max heap
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+      stones = [-s for s in stones]
+      heapify(stones)
+
+      while len(stones) > 1:
+        first = heappop(stones)
+        second = heappop(stones)
+
+        if second > first: 
+          heappush(stones, first - second)
+      
+      stones.append(0)
+
+      return abs(stones[0])
+        
+
+
+        
