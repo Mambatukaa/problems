103ea788face68b1d963c90892aeddb835d8ef12
mambatukaa <sbatuka9@gmail.com>
2024-05-07 21:26:45 -0700

    update

--- neetCode150/10.heapPriorityQueue/4.KthLargestElementInArr.215.py
+++ neetCode150/10.heapPriorityQueue/4.KthLargestElementInArr.215.py
@@ -52,3 +52,43 @@ class Solution:
       return quickSelect(0, len(nums) - 1)
 
 
+
+
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         nums.sort()
+#         return nums[len(nums) - k]
+def findKthLargest(nums: List[int], k: int) -> int:
+    def quickselect(nums, k):
+        small, equal, large = [], [], []
+        pivot = nums[len(nums) - 1]
+        for num in nums:
+            if num > pivot:
+                large.append(num)
+            elif num == pivot:
+                equal.append(num)
+            elif num < pivot:
+                small.append(num)
+
+        # CASE-1
+        if len(large) >= k:
+            # means kth largest element is present in large array
+            return quickselect(large, k)
+
+        # CASE-2
+        elif (len(large) + len(equal))  < k:
+            # this means k-th largest element is in small array
+            return quickselect(small, k-(len(large) + len(equal)))
+
+        # CASE-3
+        else:
+            # pivot (and all elems. in equal) e j aapno k-th largest number chhe.
+            return pivot
+    return quickselect(nums, k)
+
+with open('user.out', 'w') as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        k = next(inputs)
+        print(findKthLargest(nums, k), file=f)
+exit()
