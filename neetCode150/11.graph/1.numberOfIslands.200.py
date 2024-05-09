5bbba55c6543be21925e085b76f04f5f53fda9ac
mambatukaa <sbatuka9@gmail.com>
2024-05-09 11:10:18 -0700

    update

--- neetCode150/11.graph/1.numberOfIslands.200.py
+++ neetCode150/11.graph/1.numberOfIslands.200.py
@@ -52,9 +52,38 @@ solution = Solution()
 grid = [["1","0","1","1","0","1","1"]]
 
 
-print(solution.numIslands(grid))
 
-  
+# Easy dfs
+
+def numIslands(grid):
+    m = len(grid)
+    n = len(grid[0])
+
+
+    def dfs(r, c):
+        if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] != "1":
+            return
+
+        grid[r][c] = "$"
+
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    counter = 0
+
+    for r in range(m):
+        for c in range(n):
+
+            if grid[r][c] == "1":
+                counter += 1
+
+                dfs(r, c)
+    
+    return counter
+
+print(numIslands(grid))
 
 """
  
