471ed22ca440d6306bc9caf77b7e3fd16be3bff6
mambatukaa <sbatuka9@gmail.com>
2024-05-14 11:04:10 -0700

    add updated solution

--- neetCode150/11.graph/6.pacificAtlanticWaterFlow.417.py
+++ neetCode150/11.graph/6.pacificAtlanticWaterFlow.417.py
@@ -1,8 +1,11 @@
 from collections import deque
 
-# Naive
+# Naive Brute force
+# Time Complexity: O(n * m)^2
+# Space Complexity: O(n * m)^2
+# 1 hour
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+    def pacificAtlantic(self, heights):
       ROWS = len(heights)
       COLS = len(heights[0])
 
@@ -74,3 +77,82 @@ Input: heights = [
 
 
 """
+
+
+
+
+
+
+
+# Time Complexity: O(n * m)
+# Space Complexity: O(n * m)
+# Neet code algorithm
+class Solution:
+    def pacificAtlantic(self, heights):
+      ROWS = len(heights)
+      COLS = len(heights[0])
+
+
+      def dfs(r, c, seen, prev): 
+        if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in seen or heights[r][c] < prev:
+          return
+        
+        seen.add((r,c))
+
+        curr = heights[r][c]
+        
+        dfs(r + 1, c, seen, curr)
+        dfs(r - 1, c, seen, curr)
+        dfs(r, c + 1, seen, curr)
+        dfs(r, c - 1, seen, curr)
+      
+      pacific = set()
+      atlantic = set()
+
+      for c in range(COLS):
+      # Pacific ocean TOP
+        dfs(0, c, pacific, heights[0][c])
+      # Atlantic ocean RIGHT
+        dfs(ROWS - 1, c, atlantic, heights[ROWS-1][c])
+      
+      for r in range(ROWS):
+      # Pacific ocean LEFT
+        dfs(r, 0, pacific, heights[r][0])
+
+      # Atlantic ocean Bottom
+        dfs(r, COLS - 1, atlantic, heights[r][COLS-1])
+
+      res = []
+
+      for r in range(ROWS):
+          for c in range(COLS):
+              if (r,c) in pacific and (r,c) in atlantic:
+                  res.append([r,c])
+     
+      return res
+
+
+solution = Solution1()
+
+heights = [
+
+        [1,2,2,3,5],
+        [3,2,3,4,4],
+        [2,4,5,3,1],
+        [6,7,1,4,5],
+        [5,1,1,2,4]
+
+        ]
+
+print(solution.pacificAtlantic(heights))
+
+
+"""
+
+STARTED FROM OCEAN SITE AND ADD POSSIBLE VALUES TO THE EACH OCEAN
+    COMPARE OCEANS THE DUPLICATED VALUES WILL BE ANSWER..
+
+
+
+"""
+
