253766b3616f3de71ac50f0aa35d0ff46df5fc1d
mambatukaa <sbatuka9@gmail.com>
2024-05-20 17:10:44 -0500

    add solution using greedy

--- neetCode150/12.greedy/1.maxSubarray.53.py
+++ neetCode150/12.greedy/1.maxSubarray.53.py
@@ -1,1 +1,21 @@
 #initial
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+      maxSum = nums[0]
+
+      curSum = 0
+
+      for n in nums:
+        currSum += n
+
+        if currSum < 0:
+          currSum = 0
+
+        maxSum = max(maxSum, currSum)
+
+      return maxSum
+
+
