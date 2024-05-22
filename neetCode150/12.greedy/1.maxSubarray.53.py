46ee63e4da2123a3d8a8b53be9c8d930cbf17238
mambatukaa <sbatuka9@gmail.com>
2024-05-22 09:44:28 -0700

    save subarray with sum

--- neetCode150/12.greedy/1.maxSubarray.53.py
+++ neetCode150/12.greedy/1.maxSubarray.53.py
@@ -3,20 +3,33 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray(self, nums):
       maxSub = nums[0]
+      arr = [nums[0]]
+      answer = []
 
       currSum = 0
 
-      for n in nums:
+      for i in range(len(nums)):
+        n = nums[i]
+
         if currSum < 0:
           currSum = 0
+          arr = []
 
         currSum += n
+        arr.append(n)
 
-        maxSub = max(maxSub, currSum)
+        if currSum > maxSub:
+          maxSub = currSum
+          answer = arr.copy()
+
+      print(answer)
 
       return maxSub
 
 
+nums = [-2,1,-3,4,-1,2,1,-5,4];
 
+solution = Solution()
+solution.maxSubArray(nums)
