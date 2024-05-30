d018ce2d50f1247fd7d3801e27a707ca9360255e
mambatukaa <sbatuka9@gmail.com>
2024-05-30 10:05:31 -0700

    add solution using greedy approach

---
+++ neetCode150/12.greedy/maximumEvenSplit.2178.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Greedy
+class Solution:
+  def maximumEvenSplit(self, f: int):
+    ans, i = [], 2
+
+    if f % 2 == 0:
+      while i <= f:
+        ans.append(i)
+        f -= i
+        i += 2
+
+      ans[-1] += f
+    
+    return ans
+
+solution = Solution()
+print(solution.maximumEvenSplit(28))
+
