0246ebaea4aa8c3013fd3d02810e49ccb1d23fd9
mambatukaa <sbatuka9@gmail.com>
2024-06-04 22:06:00 -0700

    add solution using bit manipulation

---
+++ neetCode150/13.bitManipulation/1.singleNumber.136.py
@@ -0,0 +1,23 @@
+"""
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+"""
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Bit Manipulation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+      res = 0
+
+      for num in nums:
+        res ^= num
+
+
+      return res
+        
