5b64a40923417a6fff10f05465665e1e50a0c180
mambatukaa <sbatuka9@gmail.com>
2024-06-11 10:23:33 -0700

    add recursive solution

--- neetCode150/dp/2.minCostClimbingStairs.py
+++ neetCode150/dp/2.minCostClimbingStairs.py
@@ -1,4 +1,35 @@
+# Top to Bottom
+# Time Complexity: O
+# Space Complexity: O
 
+# Top to Bottom solution
+# Recursive
+# Memo
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+  def minCostClimbing(self, cost):
+    self.dp = {}
+
+    def helper(cost, n):
+      if n <= 1:
+        return cost[n]
+
+      if n in self.dp:
+        return self.dp[n]
+      
+      self.dp[n] = cost[n] + min(helper(cost, n - 1), helper(cost, n - 2))
+
+      return self.dp[n]
+
+
+    n = len(cost)
+
+    return min(helper(cost, n - 1), helper(cost, n - 2))
+
+solution = Solution()
+
+print(solution.minCostClimbing([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
 
 # Time Complexity: O(n)
 # Space Complexity: O(1)
@@ -16,7 +47,19 @@ def minCost(cost):
 
   return dp[-1]
 
-print(minCost([10, 15, 20]))
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Bottom To TOP
+# 
+def minCostII(cost):
+  cost.append(0)
+
+  for i in range(len(cost) - 3, -1, -1):
+    cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
+
+  return min(cost[0], cost[1])
+
+
 
 
 
