0f7a19c75e53367a399f2565286df38a55780dc5
mambatukaa <sbatuka9@gmail.com>
2024-06-13 10:18:40 -0700

    add solution using dp

---
+++ neetCode150/dp/5.longestPalindromicSubstring.py
@@ -0,0 +1,46 @@
+
+
+# DP
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+# Start from the center and expand it
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+      res = ''
+      resLength = 0
+
+      
+      for i in range(len(s)):
+        left = i
+        right = i
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+          if right - left + 1 > resLength:
+            resLength = right - left + 1
+            res = s[left:right+1]
+
+          left -= 1
+          right += 1
+      
+      for i in range(len(s)):
+        left = i
+        right = i + 1
+
+        while left >= 0 and right < len(s):
+          if s[left] != s[right]:
+            break
+
+          if right - left + 1 > resLength:
+            resLength = right - left + 1
+            res = s[left:right+1]
+
+          left -= 1
+          right += 1
+        
+      return res
+
+
+solution = Solution()
+
+print(solution.longestPalindrome("abbbaaac"))
+        
