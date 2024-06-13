d1686cfa42b3bfc0631691e4aad8b843286c1e80
mambatukaa <sbatuka9@gmail.com>
2024-06-13 12:52:53 -0700

    refactor

--- neetCode150/dp/6.longestPalindromicSubstrings.py
+++ neetCode150/dp/6.longestPalindromicSubstrings.py
@@ -1,35 +1,28 @@
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 # DP
-# Ruse a previously computed palindrome
+# Reuse a previously computed palindrome
 class Solution:
+    def countPali(self, s, l, r):
+      counter = 0
+
+      while l >= 0 and r < len(s) and s[l] == s[r]:
+        counter += 1
+
+        l -= 1
+        r += 1
+
+      return counter
+
     def countSubstrings(self, s: str) -> int:
 
       # check palindromic on every substring
-
       counter = 0
 
       for i in range(len(s)):
-        l = i
-        r = i
+        counter += self.countPali(s, i, i)
+        counter += self.countPali(s, i, i + 1)
 
-        # is in range
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-          counter += 1
-
-          l -= 1
-          r += 1
-
-        l = i
-        r = i + 1
-
-        # is in range
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-          counter += 1
-
-          l -= 1
-          r += 1
-      
       return counter
 
 solution = Solution()
