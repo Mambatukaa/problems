bbdcb4828c9d5c63fc6363a4b292e064583a82ab
mambatukaa <sbatuka9@gmail.com>
2024-06-25 13:00:10 -0700

    add solution using GRAPH bfs

---
+++ dsaLeetCode/treesAndGraph/graph/implicitGraphs/4.jumpGameIII.1306.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def jumpGameIII(arr, start):
+  n = len(arr)
+  seen = [False] * n
+
+  seen[start] = True
+
+  q = deque([start])
+
+  while q:
+    currIdx = q.popleft()
+
+    if arr[currIdx] == 0:
+      return True
+
+    first = currIdx + arr[currIdx] 
+    second = currIdx - arr[currIdx] 
+
+    # add neighbors check neighbor is valid or not
+    if first < n and seen[first] == False:
+      seen[first] = True
+      q.append(first)
+
+    if second >= 0 and seen[second] == False:
+      seen[second] = True
+      q.append(second)
+
+  return False
+
+arr = [4, 2, 3, 0, 3, 1, 2]
+start = 0
+
+print('res:', jumpGameIII(arr, start))
+
+
+"""
+
+non-negative inteegers
+
+you can jump to i + arr[i] or i - arr[i]
+
+       0  1  2  3  4  5  6
+arr = [4, 2, 3, 0, 3, 1, 2]
+start = 0
+
+
+i = 0
+  can jump 0 + arr[0] ===> 4
+  can jump 0 - arr[0] ===> -4 not valid
+
+
+i = 4
+  can jump => 4 + arr[4] => 4 + 3 ===> 7 not valid
+  can jump => 4 - arr[4] => 4 - 3 ===> 1
+
+
+i = 1
+
+  can jump => 1 + arr[1] => 1 + 2 ==> 3 GOTCHA arr[3] == 0 return TRUE
+
+
+
+# GRAPH BFS problem
+
+1. Add possible jumping idx to the queue starts from starting point
+2. Add the neighbors until reach the 0
+3. If q become empty return False
+
+
+"""
