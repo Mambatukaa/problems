2a8f0303baee8d41575c08756f6e21a35000108e
mambatukaa <sbatuka9@gmail.com>
2024-07-01 22:37:57 -0700

    add optimal solution

--- amazon/leetCode/ArraysNStrings/12.minWindowSubstring.py
+++ amazon/leetCode/ArraysNStrings/12.minWindowSubstring.py
@@ -43,8 +43,6 @@ def minimumWindow(s, t):
 s = "ADOBECODEBANC"
 t = "ABC"
 
-print(minimumWindow(s, t))
-
 
 
 """
@@ -78,3 +76,57 @@ s and t consist of upper and lower english letters.
 Find algorithm runs in O(m + n) time.
 
 """
+
+
+# Time Complexity: O(m + n)
+# Space Complexity: O(m + n)
+def minWindow(s, t):
+  countT, window = {}, {}
+
+  for c in t:
+    countT[c] = countT.get(c, 0) + 1
+
+
+  l = 0
+
+  res = [-1, -1]
+  resLength = float('inf')
+
+  have, need = 0, len(countT)
+
+  for r in range(len(s)):
+    c = s[r]
+
+    window[c] = window.get(c, 0) + 1
+
+    if c in countT and window[c] == countT[c]:
+      have += 1
+
+    # window has all s elements
+    while have == need:
+      if (r - l + 1) < resLength:
+        res = [l, r]
+        resLength = r - l + 1
+
+      # pop from the left of our window
+      window[s[l]] -= 1
+
+      if s[l] in countT and window[s[l]] < countT[s[l]]:
+        have -= 1
+
+      l += 1
+
+  l, r = res
+
+  return s[l: r + 1] if res != float('inf') else ""
+        
+  # need, have
+
+  # if need == have shrink the window
+
+s = "ADOBECODEBANC"
+t = "ABC"
+
+print(minWindow(s, t))
+
+
