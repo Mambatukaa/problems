22f1a451f99de02ac0e7df4c303662e3b882617e
mambatukaa <sbatuka9@gmail.com>
2024-07-02 22:25:15 -0700

    update

--- amazon/leetCode/ArraysNStrings/13.compareVersionNumbers.py
+++ amazon/leetCode/ArraysNStrings/13.compareVersionNumbers.py
@@ -24,12 +24,9 @@ def versionNumbersII(version1, version2):
   v2 = [int(i) for i in version2.split('.')]
 
   diff = len(v1) - len(v2)
-  print(diff, v1, v2)
+
   v1 += [0] * -diff
   v2 += [0] * diff
-
-  print("------")
-  print(v1, v2)
 
   return (v1 > v2) - (v1 < v2)
   
