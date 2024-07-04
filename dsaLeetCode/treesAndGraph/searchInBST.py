75bdf72a31a4f712c4e8c67e252a15976402c240
mambatukaa <sbatuka9@gmail.com>
2024-07-04 12:58:24 -0700

    add solution using iteration

---
+++ dsaLeetCode/treesAndGraph/searchInBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time Complexity: O(log n) # if tree is balanced
+# Space Complexity: O(1)
+class Solution:
+    def searchBST(self, root, val):
+      # left or right
+
+      while root:
+        if root.val == val:
+          return root
+
+        if root.val < val:
+          # go right
+          root = root.right
+        else:
+          # go left
+          root = root.left
+
+      return None
