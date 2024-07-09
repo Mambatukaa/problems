b404e34ac8e344645df19a487cc8b30f3ad59e2e
mambatukaa <sbatuka9@gmail.com>
2024-07-08 22:32:50 -0700

    add solution using replace function

--- amazon/leetCode/ArraysNStrings/mostCommonWord.py
+++ amazon/leetCode/ArraysNStrings/mostCommonWord.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 import re
 # Time Complexity: O(n)
 # Space Complexity: O(n)
@@ -9,7 +9,17 @@ def mostCommonWord(paragraph, banned):
   
   return Counter(w for w in words if w not in bannedSet).most_common(1)[0][0]
 
+def mostCommonWordII(paragraph, banned):
+  bannedSet = set(banned)
+
+  for i in "!.',;:?": 
+    paragraph = paragraph.replace(i, ' ')
+
+  words = paragraph.lower().split()
+  
+  return Counter(w for w in words if w not in bannedSet).most_common(1)[0][0]
+
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = ["hit"]
 
-mostCommonWord(paragraph, banned)
+print("res:", mostCommonWordII(paragraph, banned))
