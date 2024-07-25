fab5259b5f9b4e89b3031a974ed8a6877a5822af
mambatukaa <sbatuka9@gmail.com>
2024-07-25 12:16:18 -0700

    Initial TME

---
+++ amazon/leetCode/treesAndGraphs/6.wordLadderII.py
@@ -0,0 +1,107 @@
+import string
+from collections import deque, defaultdict
+def wordLadderII(beginWord, endWord, wordList):
+  # since we are check newWord from wordList every time. Convert wordList to set to improve time complexity
+  wordListSet = set(wordList)
+  res = []
+
+  if endWord not in wordListSet:
+    return res
+
+
+  # NOTE
+  seqLength = float('inf')
+
+  q = deque([[beginWord, []]])
+
+  while q:
+    curr, currSeq = q.popleft()
+    currSeq.append(curr)
+
+    # add valid curr neighbors to the queue
+    for i in range(len(curr)):
+
+      for combination in string.ascii_lowercase:
+        neighbor = curr[:i] + combination + curr[i+1:]
+
+        # NOTE vaild neighbors
+        if neighbor in wordListSet and neighbor not in currSeq:
+
+          # NOTE transformation completed
+          if neighbor == endWord:
+            currSeq.append(endWord)
+            n = len(currSeq)
+
+            # Transformation end
+            if n < seqLength:
+              res = [currSeq]
+              seqLength = n
+
+            elif n == seqLength:
+              res.append(currSeq)
+              seqLength = n
+
+            continue
+
+          q.append([neighbor, currSeq.copy()])
+
+  return res
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot" ,"dog","lot","log","cog"]
+
+beginWord = "aaaaa"
+endWord = "ggggg"
+
+wordList = ["aaaaa","caaaa","cbaaa","daaaa","dbaaa","eaaaa","ebaaa","faaaa","fbaaa","gaaaa","gbaaa","haaaa","hbaaa","iaaaa","ibaaa","jaaaa","jbaaa","kaaaa","kbaaa","laaaa","lbaaa","maaaa","mbaaa","naaaa","nbaaa","oaaaa","obaaa","paaaa","pbaaa","bbaaa","bbcaa","bbcba","bbdaa","bbdba","bbeaa","bbeba","bbfaa","bbfba","bbgaa","bbgba","bbhaa","bbhba","bbiaa","bbiba","bbjaa","bbjba","bbkaa","bbkba","bblaa","bblba","bbmaa","bbmba","bbnaa","bbnba","bboaa","bboba","bbpaa","bbpba","bbbba","abbba","acbba","dbbba","dcbba","ebbba","ecbba","fbbba","fcbba","gbbba","gcbba","hbbba","hcbba","ibbba","icbba","jbbba","jcbba","kbbba","kcbba","lbbba","lcbba","mbbba","mcbba","nbbba","ncbba","obbba","ocbba","pbbba","pcbba","ccbba","ccaba","ccaca","ccdba","ccdca","cceba","cceca","ccfba","ccfca","ccgba","ccgca","cchba","cchca","cciba","ccica","ccjba","ccjca","cckba","cckca","cclba","cclca","ccmba","ccmca","ccnba","ccnca","ccoba","ccoca","ccpba","ccpca","cccca","accca","adcca","bccca","bdcca","eccca","edcca","fccca","fdcca","gccca","gdcca","hccca","hdcca","iccca","idcca","jccca","jdcca","kccca","kdcca","lccca","ldcca","mccca","mdcca","nccca","ndcca","occca","odcca","pccca","pdcca","ddcca","ddaca","ddada","ddbca","ddbda","ddeca","ddeda","ddfca","ddfda","ddgca","ddgda","ddhca","ddhda","ddica","ddida","ddjca","ddjda","ddkca","ddkda","ddlca","ddlda","ddmca","ddmda","ddnca","ddnda","ddoca","ddoda","ddpca","ddpda","dddda","addda","aedda","bddda","bedda","cddda","cedda","fddda","fedda","gddda","gedda","hddda","hedda","iddda","iedda","jddda","jedda","kddda","kedda","lddda","ledda","mddda","medda","nddda","nedda","oddda","oedda","pddda","pedda","eedda","eeada","eeaea","eebda","eebea","eecda","eecea","eefda","eefea","eegda","eegea","eehda","eehea","eeida","eeiea","eejda","eejea","eekda","eekea","eelda","eelea","eemda","eemea","eenda","eenea","eeoda","eeoea","eepda","eepea","eeeea","ggggg","agggg","ahggg","bgggg","bhggg","cgggg","chggg","dgggg","dhggg","egggg","ehggg","fgggg","fhggg","igggg","ihggg","jgggg","jhggg","kgggg","khggg","lgggg","lhggg","mgggg","mhggg","ngggg","nhggg","ogggg","ohggg","pgggg","phggg","hhggg","hhagg","hhahg","hhbgg","hhbhg","hhcgg","hhchg","hhdgg","hhdhg","hhegg","hhehg","hhfgg","hhfhg","hhigg","hhihg","hhjgg","hhjhg","hhkgg","hhkhg","hhlgg","hhlhg","hhmgg","hhmhg","hhngg","hhnhg","hhogg","hhohg","hhpgg","hhphg","hhhhg","ahhhg","aihhg","bhhhg","bihhg","chhhg","cihhg","dhhhg","dihhg","ehhhg","eihhg","fhhhg","fihhg","ghhhg","gihhg","jhhhg","jihhg","khhhg","kihhg","lhhhg","lihhg","mhhhg","mihhg","nhhhg","nihhg","ohhhg","oihhg","phhhg","pihhg","iihhg","iiahg","iiaig","iibhg","iibig","iichg","iicig","iidhg","iidig","iiehg","iieig","iifhg","iifig","iighg","iigig","iijhg","iijig","iikhg","iikig","iilhg","iilig","iimhg","iimig","iinhg","iinig","iiohg","iioig","iiphg","iipig","iiiig","aiiig","ajiig","biiig","bjiig","ciiig","cjiig","diiig","djiig","eiiig","ejiig","fiiig","fjiig","giiig","gjiig","hiiig","hjiig","kiiig","kjiig","liiig","ljiig","miiig","mjiig","niiig","njiig","oiiig","ojiig","piiig","pjiig","jjiig","jjaig","jjajg","jjbig","jjbjg","jjcig","jjcjg","jjdig","jjdjg","jjeig","jjejg","jjfig","jjfjg","jjgig","jjgjg","jjhig","jjhjg","jjkig","jjkjg","jjlig","jjljg","jjmig","jjmjg","jjnig","jjnjg","jjoig","jjojg","jjpig","jjpjg","jjjjg","ajjjg","akjjg","bjjjg","bkjjg","cjjjg","ckjjg","djjjg","dkjjg","ejjjg","ekjjg","fjjjg","fkjjg","gjjjg","gkjjg","hjjjg","hkjjg","ijjjg","ikjjg","ljjjg","lkjjg","mjjjg","mkjjg","njjjg","nkjjg","ojjjg","okjjg","pjjjg","pkjjg","kkjjg","kkajg","kkakg","kkbjg","kkbkg","kkcjg","kkckg","kkdjg","kkdkg","kkejg","kkekg","kkfjg","kkfkg","kkgjg","kkgkg","kkhjg","kkhkg","kkijg","kkikg","kkljg","kklkg","kkmjg","kkmkg","kknjg","kknkg","kkojg","kkokg","kkpjg","kkpkg","kkkkg","ggggx","gggxx","ggxxx","gxxxx","xxxxx","xxxxy","xxxyy","xxyyy","xyyyy","yyyyy","yyyyw","yyyww","yywww","ywwww","wwwww","wwvww","wvvww","vvvww","vvvwz","avvwz","aavwz","aaawz","aaaaz"]
+
+beginWord = "red"
+
+endWord = "tax"
+
+wordList = ["ted", "tex", "red" ,"tax","tad","den", "rex" ,"pee"]
+
+
+
+
+print("res:", wordLadderII(beginWord, endWord, wordList))
+
+
+"""
+
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord 
+-> s1 -> s2 -> ... -> sk such that:
+
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return all the shortest 
+transformation sequences from beginWord to endWord, or an empty list if no such sequence exists. 
+Each sequence should be returned as a list of the words [beginWord, s1, s2, ..., sk].
+
+
+
+Example 1:
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot" ,"dog","lot","log","cog"].
+Output: [["hit", "hot","dot","dog", "cog"], ["hit", "hot","lot","log". ,"cog" ]]
+
+Explanation: There are 2 shortest transformation sequences:
+"hit" → "hot" →> "dot" →> "dog" → "cog"
+"hit" → "hot" →> "lot" →> "log" →> "cog"
+
+
+beginWord does not to be in wordList.
+endWord does neet to be in wordList.
+
+
+newWord must be in wordList
+
+
+currentWord can change only one letter at a time.
+
+
+
+"""
