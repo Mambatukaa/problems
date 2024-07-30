45a8340bd0ade9841e2ff195034b261f91122967
mambatukaa <sbatuka9@gmail.com>
2024-07-29 21:35:53 -0700

    initial

---
+++ amazon/leetCode/treesAndGraphs/12.cutOffTreesForGolfEvent.py
@@ -0,0 +1,21 @@
+"""
+
+
+
+You are asked to cut off all the trees in a forest for a golf event. The forest is represented as an m x n
+matrix. In this matrix:
+• 0 means the cell cannot be walked through.
+• 1 represents an empty cell that can be walked through.
+• A number greater than 1 represents a tree in a cell that can be walked through, and this number is the
+tree's height.
+
+
+1 2 3
+9 7 8 ====> 12
+101112 
+
+
+Cut the trees from shortest to tallest. Find the minimum path to cut all of trees.
+
+
+"""
