8d004695d6c6755797659abc9525cc455b1744a6
mambatukaa <sbatuka9@gmail.com>
2024-07-31 12:49:49 -0700

    init

---
+++ amazon/leetCode/recursive/2.generateParentheses.py
@@ -0,0 +1,37 @@
+def generateParenthesis()
+"""
+
+Given n pairs of parentheses, write a function to generate all combinations.
+
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+
+
+Constraints:
+  1 <= n <= 8
+
+
+
+opening = 0
+closing = 0
+
+
+1. ( ( ( ) ) )
+
+
+if opening 
+
+while opening == n:
+  p = curr.pop()
+
+  if p == (:
+    opening -= 1
+  else:
+    closing -= 1
+
+
+2. ( ( ( ) ) )
+
+"""
