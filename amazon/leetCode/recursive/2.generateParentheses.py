c2b9ca75aa74cd513f2a4620cb7b9869b7ad9948
mambatukaa <sbatuka9@gmail.com>
2024-07-31 21:09:52 -0700

    add solution using backtracking

--- amazon/leetCode/recursive/2.generateParentheses.py
+++ amazon/leetCode/recursive/2.generateParentheses.py
@@ -1,4 +1,30 @@
-def generateParenthesis()
+# Time Complexity: O(2^n)
+# Space Complexity: O(2^n)
+def generateParenthesis(n):
+  stack = []
+  res = []
+
+  def backtrack(openN, closedN):
+    if openN == closedN == n:
+      res.append("".join(stack))
+      return
+
+    if openN < n:
+      stack.append("(")
+      backtrack(openN + 1, closedN)
+      stack.pop()
+
+    if closedN < openN:
+      stack.append(")")
+      backtrack(openN, closedN + 1)
+      stack.pop()
+
+  backtrack(0, 0)
+
+  return res
+
+print("res:", generateParenthesis(3))
+
 """
 
 Given n pairs of parentheses, write a function to generate all combinations.
