2e39ff3558ecc05affcfb01da4fe6500f9da7490
mambatukaa <sbatuka9@gmail.com>
2024-08-05 15:39:24 -0500

    update

--- amazon/leetCode/recursive/4.wordSearchII.py
+++ amazon/leetCode/recursive/4.wordSearchII.py
@@ -1,5 +1,4 @@
-
-
+from trie import Trie
 
 # TIME LIMIT EXCEEDED
 # Time complexity: O(n * m * k) k = length of words
@@ -7,7 +6,6 @@
 def wordSearchII(board, words):
   m = len(board)
   n = len(board[0])
-
 
   seen = set()
 
@@ -36,26 +34,92 @@ def wordSearchII(board, words):
 
     return res
 
-
-
   res = set()
 
   for row in range(m):
     for col in range(n):
-
       for word in words:
         seen = set()
-
         if board[row][col] == word[0] and dfs(row, col, 0, word):
           res.add(word)
 
   return list(res)
 
-board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]] 
+
+
+def wordSearch(board, words):
+  trie = Trie()
+
+  firstLetters = set()
+
+  # add words in trie
+  for word in words:
+    firstLetters.add(word[0])
+    trie.insert(word)
+
+
+  # row length
+  m = len(board)
+
+  # col length
+  n = len(board[0])
+
+  # response
+  res = []
+  seen = set()
+
+  def dfs(row, col, node, curr):
+    if node.endOfString:
+      res.append(curr)
+      node.endOfString = False
+
+      
+    if (row < 0 or 
+        col < 0 or 
+        row == m or 
+        col == n or
+        (row, col) in seen
+        ):
+      return
+
+    seen.add((row, col))
+
+    tmp = board[row][col]
+    node = node.children.get(tmp)
+
+    if not node:
+      return
+    
+    dfs(row + 1, col, node, curr + tmp)
+    dfs(row - 1, col, node, curr + tmp)
+    dfs(row, col + 1, node, curr + tmp)
+    dfs(row, col - 1, node, curr + tmp)
+  
+  node = trie.root
+
+  for row in range(m):
+    for col in range(n):
+      letter = board[row][col]
+      if letter in firstLetters:
+        seen = set()
+        dfs(row, col, node, "")
+      
+
+  return res
+
+board = [["o","a","a","n"],
+         ["e","t","a","e"],
+         ["i","h","k","r"],
+         ["i","f","l","v"]
+         ] 
 words = ["oath","pea","eat","rain"]
 
 
-print("res:", wordSearchII(board, words))
+board = ["a", "a"]
+words = ["aa"]
+
+
+print("res:", wordSearch(board, words))
 
 """
 
@@ -80,6 +144,12 @@ Output: ["eat","oath"]
 
 
 
+
+
+# Add words to the TRIE
+
+# And do the DFS on each node and track the words
+# If word is found add to the result
 
 
 """
