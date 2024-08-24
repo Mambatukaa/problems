08f11c834df9a765716ecdbc3193e83832d9052d
mambatukaa <sbatuka9@gmail.com>
2024-08-23 21:24:22 -0700

    initial

---
+++ amazon/leetCode/dynamicProgramming/1.longestPalindromicSubstring.py
@@ -0,0 +1,16 @@
+def longestPalindromic(s):
+  print(s)
+
+"""
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+
+Constraints:
+• 1 <= s. length <= 1000
+• s consist of only digits and English letters.
+
+
+"""
