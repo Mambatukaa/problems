ec382d89ed3ca0d4256a9f21f406d1d1047f0cc4
mambatukaa <sbatuka9@gmail.com>
2024-09-25 10:31:05 -0700

    add quick sort

---
+++ course/algorithm/divideAndConquer/quickSort.py
@@ -0,0 +1,38 @@
+
+# Time Complexity: O(N log N)... Worst: O(n^2)
+# Space Complexity: O(1) Unlike merge sort
+
+"""
+Choose pivot number and locate smaller numbers at the left of the pivot and larger numbers at the right of the pivot.
+
+"""
+
+
+def partition(nums, start, end):
+  pivot = end
+  i = start - 1
+
+  for j in range(start, end + 1):
+    if nums[j] <= nums[pivot]:
+      i += 1
+      nums[j], nums[i] = nums[i], nums[j]
+
+  return i
+
+# Time Complexity: O(N log N)
+# Space Complexity: O(1)
+def quickSort(nums, start, end):
+  if start < end:
+    pivot = partition(nums, start, end)
+
+    # left sub array
+    quickSort(nums, start, pivot - 1)
+
+    # right sub array
+    quickSort(nums, pivot + 1, end)
+
+nums = [3, 5, 8, 1, 2, 9, 4, 7, 6]
+
+quickSort(nums, 0, len(nums) - 1)
+
+print("res:", nums)
