de4d0bd7a83469298f7807c478625b0930b3be1e
mambatukaa <sbatuka9@gmail.com>
2024-10-15 21:13:25 -0700

    add description

--- dsaLeetCode/dp/solvingQuestionsWithBrainpower.py
+++ dsaLeetCode/dp/solvingQuestionsWithBrainpower.py
@@ -1,4 +1,4 @@
-def solvingQuestions():
+def solvingQuestions(questions):
   print(111)
 """
 
@@ -13,5 +13,18 @@ questions. If you skip question i, you get to make the decision on the next ques
 • If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you will be unable to solve questions 2 and 3.
 Return the maximum points you can earn for the exam.
 
+
+
+
+
+Example 1:
+Input: questions = [[3,2], [4,3], [4,4], [2,5]]
+Output: 5
+
+Explanation: The maximum points can be earned by solving questions @ and 3.
+- Solve question 0: Earn 3 points, will be unable to solve the next 2 questions
+- Unable to solve questions 1 and 2
+- Solve question 3: Earn 2 points
+Total points earned: 3 + 2 = 5. There is no other way to earn 5 or more points.
 
 """
