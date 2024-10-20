87a657cda8d2eaf09bf91b4cd1802127c964892a
mambatukaa <sbatuka9@gmail.com>
2024-10-20 13:30:48 -0700

    add dp solutions

--- dsaLeetCode/dp/coinChange.py
+++ dsaLeetCode/dp/coinChange.py
@@ -1,33 +1,98 @@
-# Backtracking solution 
-# Time Complexity: O(2^n)
+
+
+# Brute force BACKTRACKING
+# Leetcode solution
+# Time Complexity: O(n^s)
+# Space Complexity: O(n)
+# Time LIMIT EXCEEDED
 def coinChange(coins, amount):
-  coins.sort(reverse=True)
+  def dfs(remain):
+    if remain < 0:
+      return -1
+    if remain == 0:
+      return 0
 
-
-  def backtracking(curr, currSum):
-    if currSum == amount:
-      return True
-      
-
-    if currSum > amount:
-      return False
-
-
+    minCount = float('inf')
     for coin in coins:
-      curr.append(coin)
-      res = backtracking(curr, currSum + coin)
+      count = dfs(remain - coin)
 
-      if res:
-        return curr
+      # wrong path
+      if count == -1:
+        continue
+      
+      minCount = min(minCount, count + 1)
+    
+    return -1 if minCount == float('inf') else minCount
 
-      curr.pop()
+  return dfs(amount)
+
+# Backtracking solution 
+# Top Down
+# Memo
+# Time Complexity: O(n * S)
+# Space Complexity: O(S)
+def coinChangeII(coins, amount):
+  def dfs(remain):
+    if remain in memo:
+      return memo[remain]
+
+    if remain < 0:
+      return -1
+    if remain == 0:
+      return 0
+
+    minCount = float('inf')
+    for coin in coins:
+      count = dfs(remain - coin)
+
+      # wrong path
+      if count == -1:
+        continue
+      
+      minCount = min(minCount, count + 1)
+    
+    memo[remain] = -1 if minCount == float('inf') else minCount
+
+    return memo[remain]
+
+  memo = {}
+  return dfs(amount)
+
+print("res 1:", coinChange([1, 2, 5], 11))
+print("res 2:", coinChangeII([1, 2, 5], 11))
+
+# Dynamic programming
+# Bottom UP neetcode
+def coinChangeIII(coins, amount):
+  dp = [amount + 1] * (amount + 1)
+  dp[0] = 0
+
+  for a in range(1, amount + 1):
+    for coin in coins:
+      if a - coin >= 0:
+        dp[a] = min(dp[a], 1 + dp[a - coin])
 
 
-  return len(backtracking([], 0))
+  if dp[amount] == amount + 1:
+    return -1
+
+  return dp[amount]
+
+# Bottom UP solution iterative
+def coinChangeIV(coins, amount):
+  dp = [float('inf')] * (amount + 1)
+  dp[0] = 0
+
+  for coin in coins:
+    for a in range(coin, amount + 1):
+      dp[a] = min(dp[a], dp[a - coin] + 1)
+
+  return dp[amount] if dp[amount] != float('inf') else -1
 
 
+# coin = 4, a = 7, dp[7] = min(dp[7], 1 + dp[7-4])
 
-print("res:", coinChange([1,2,5], 11))
+print("res:", coinChangeIV([1, 2, 5], 11))
 """
 
 You are given an integer array coins representing coins of different denominations and an integer amount representing a total
