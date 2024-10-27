b0045734d911c3271cb6adfb82d549ba1a8d5cea
mambatukaa <sbatuka9@gmail.com>
2024-10-26 21:21:23 -0700

    add solution

--- microsoft/1.arraysNStrings/3.stringToInteger.py
+++ microsoft/1.arraysNStrings/3.stringToInteger.py
@@ -1,3 +1,36 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def myAtoi(self, s: str) -> int:
-        
+
+      s = s.strip()
+
+      if not s:
+        return 0
+
+
+      sign = -1 if s[0] == "-" else 1
+
+      idx = 1 if s[0] in ["+", "-"] else 0
+
+      res = 0
+
+      for i in range(idx, len(s)):
+        ch = s[i]
+
+        if ch not in "0123456789":
+          break
+
+        res *= 10
+        res += int(ch)
+
+      res *= sign
+
+
+      if res > 2**31 - 1:
+        return 2**31 - 1
+      elif res < -2**31:
+        return -2**31
+      else:
+        return res
+
