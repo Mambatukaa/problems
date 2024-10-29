1b5bfedbd4738e599b4d1ed55b66375230edca30
mambatukaa <sbatuka9@gmail.com>
2024-10-28 22:35:07 -0700

    add solution using two pointers

--- microsoft/1.arraysNStrings/4.reverseString.py
+++ microsoft/1.arraysNStrings/4.reverseString.py
@@ -1,6 +1,23 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def reverseString(self, s):
+      l = 0
+      r = len(s) - 1
+
+      while l < r:
+        s[l], s[r] = s[r], s[l]
+
+        l += 1
+        r -= 1
+
+
+
+solution = Solution()
+
+s = ["h","e","l","l","o"]
+s = ["H","a","n","n","a","h"]
+
+solution.reverseString(s)
         
+print(s)
