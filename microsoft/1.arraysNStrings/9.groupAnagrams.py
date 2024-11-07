206160aef0fa16154cdc93fcfd204e7f90d11761
mambatukaa <sbatuka9@gmail.com>
2024-11-06 20:48:43 -0800

    add optimal solution using counter

--- microsoft/1.arraysNStrings/9.groupAnagrams.py
+++ microsoft/1.arraysNStrings/9.groupAnagrams.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # Time Complexity: O(n * m log m)
 # Space Complexity: O(k)
 # Add solution using map
@@ -8,14 +10,34 @@ def groupAnagrams(strs):
     sortedStr = ''.join(sorted(item))
     
     if not sortedStr in res:
-      res[sortedStr] = [item]
-    else:
-      res[sortedStr].append(item)
+      res[sortedStr] = []
+
+    res[sortedStr].append(item)
 
   return list(res.values())
 
 
-print("res:", groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+# Time Complexity: O(n * m)
+# Space Complexity: O(n)
+def groupAnagramsII(strs):
+  res = {}
+  # res = defaultdict(list)
+
+  for word in strs:
+    counter = [0] * 26 
+
+    for ch in word:
+      counter[ord(ch) - ord("a")] += 1
+
+    if tuple(counter) not in res:
+      res[tuple(counter)] = []
+
+    res[tuple(counter)].append(word)
+
+  return list(res.values())
+
+
+print("res:", groupAnagramsII(["eat", "tea", "tan", "ate", "nat", "bat"]))
 
 """
 Example 1:
