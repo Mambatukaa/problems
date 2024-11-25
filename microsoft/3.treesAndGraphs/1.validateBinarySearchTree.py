a3453a3ecec010b28b9c4644bd04e1f8fbe44480
mambatukaa <sbatuka9@gmail.com>
2024-11-25 08:57:38 -0800

    add solution using min and max values

--- microsoft/3.treesAndGraphs/1.validateBinarySearchTree.py
+++ microsoft/3.treesAndGraphs/1.validateBinarySearchTree.py
@@ -50,14 +50,50 @@ class Solution:
 
     return True
 
+  # with min and max value
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def isValidBSTIII(self, root):
+    def isValid(minVal, root, maxVal):
+      if not root:
+        return True
+
+      if minVal >= root.val or maxVal <= root.val:
+        return False
+
+      return isValid(minVal, root.left, root.val) and isValid(root.val, root.right, maxVal)
+    
+    return isValid(float('-inf'), root, float("inf"))
+
+  # BST
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def isValidBSTIV(self, root):
+    stack = [[float('-inf'), root, float("inf")]]
+
+    while stack:
+      minVal, curr, maxVal = stack.pop()
+
+      if minVal >= curr.val or maxVal <= curr.val:
+        return False
+
+      if curr.left:
+        stack.append([minVal, curr.left, curr.val])
+
+      if curr.right:
+        stack.append([curr.val, curr.right, maxVal])
+
+    return True
+
+
 
 root = TreeNode(2)
-node2 = TreeNode(1)
-node3 = TreeNode(3)
+node2 = TreeNode(2)
+node3 = TreeNode(2)
 
 
 root.left = node2
 root.right = node3
 
 solution = Solution()
-print("Res:", solution.isValidBSTII(root))
+print("Res:", solution.isValidBSTIV(root))
