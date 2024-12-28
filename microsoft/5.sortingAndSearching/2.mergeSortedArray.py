d6a913b5de9972ba70a5001a1b5f5e76f7b15768
mambatukaa <sbatuka9@gmail.com>
2024-12-27 22:23:39 -0800

    add solution using 3 pointers

--- microsoft/5.sortingAndSearching/2.mergeSortedArray.py
+++ microsoft/5.sortingAndSearching/2.mergeSortedArray.py
@@ -1,14 +1,50 @@
 
+# Naive approach
+# Merge and sort
+
+
+
+# Merge from the start
+# Time Complexity: O(n + m)
+# Space Complexity: O(n)
 def merge(nums1, m, nums2, n):
-    #empty slot
+    nums1Copy = nums1[:m]
+    p1 = 0
+    p2 = 0
+
+    for p in range(n + m):
+        if p2 >= n or (p1 < m and nums1Copy[p1] <= nums2[p2]):
+            nums1[p] = nums1Copy[p1]
+            p1 += 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 += 1
+
+# Merge from the end
+# Time Complexity: O(n + m)
+# Space Complexity: O(1)
+def mergeII(nums1, m, nums2, n):
+    p1 = m - 1
+    p2 = n - 1
+
+    for p in range(n + m - 1, -1, -1):
+        if p2 < 0:
+            break
+
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
 
 
-
-nums1 = [1,2,3,0,0,0,0]
-m = 3
-nums2 = [2,2,5,6]
-n = 4
-merge(nums1, m, nums2, n)
+nums1 = [1,2,3,4,0]
+# nums1Copy = [1, 2, 3]
+m = 4
+nums2 = [1]
+n = 1
+mergeII(nums1, m, nums2, n)
 print("res:", nums1)
 
 
