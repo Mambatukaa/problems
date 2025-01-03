c4b04723c19dd13483d2c302901a480cbbf7a6bb
mambatukaa <sbatuka9@gmail.com>
2025-01-02 16:22:25 -0800

    up

---
+++ microsoft/5.sortingAndSearching/5.findMinInRotateddSortedArrayII.py
@@ -0,0 +1,12 @@
+"""
+Sorted array may contains duplicated values, return the minimum element of this array.
+
+You must write an algorithm that runs in 0 (log n) time.
+
+
+"""
+def findMin(nums):
+nums = [4, 5, 1, 2, 3]
+
+print("res:", findMinII(nums))
+
