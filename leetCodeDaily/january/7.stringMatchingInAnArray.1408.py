83de664ecdc2e9b11b66b5ab5bbd9de1ad0860df
mambatukaa <sbatuka9@gmail.com>
2025-01-07 22:10:35 -0800

    add naive solution

---
+++ leetCodeDaily/january/7.stringMatchingInAnArray.1408.py
@@ -0,0 +1,42 @@
+
+"""
+
+Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+A substring is a contiguous sequence of characters within a string
+
+Example 1:
+    Input: words = ["mass", "as", "hero", "superhero"]
+    Output: ["as", "hero"]
+
+    Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+
+
+
+"""
+
+
+# Naive approach
+# Brute force
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+def stringMatching(words):
+    res = []
+
+    for currWord in words:
+
+        for otherWord in words:
+            if currWord == otherWord:
+                continue
+
+            if currWord in otherWord:
+                res.append(currWord)
+                break
+
+    return res
+
+
+
+words = ["mass", "as", "hero", "superhero"]
+
+
+print("res:", stringMatching(words))
