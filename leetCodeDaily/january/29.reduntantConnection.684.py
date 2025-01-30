bd55b10bb55e0d955ade6a01bd2d7c9cef15f32b
mambatukaa <sbatuka9@gmail.com>
2025-01-30 09:29:59 -0800

    add solution using union find

--- leetCodeDaily/january/29.reduntantConnection.684.py
+++ leetCodeDaily/january/29.reduntantConnection.684.py
@@ -89,3 +89,54 @@ solution = Solution()
 edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
 
 print("res:", solution.findRedundantConnection(edges))
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank = {}
+
+        for i in range(1, n + 1):
+            self.parent[i] = i
+            self.rank[i] = 0
+
+
+    def find(self, n):
+        p = self.parent[n]
+
+        # find the root parent
+        while p != self.parent[p]:
+            # optimization connect with grandparent
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+
+        return p
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        # cycle detected 
+        if p1 == p2:
+            return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+
+
+        return True
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Union find solution
+class Solution:
+    def findRedundantConnection(self, edges):
+        unionFind = UnionFind(len(edges))
+
+        for n1, n2 in edges:
+            if not unionFind.union(n1, n2):
+                return [n1, n2]
+        
