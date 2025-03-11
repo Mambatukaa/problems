8f043f891edd93224609385515ccfdfe8383200e
mambatukaa <sbatuka9@gmail.com>
2025-03-10 21:05:31 -0700

    add solutions

--- leetCodeDaily/march/8.minimumRecolorsToGetKConsecutiveBlackBlocks.2379.py
+++ leetCodeDaily/march/8.minimumRecolorsToGetKConsecutiveBlackBlocks.2379.py
@@ -34,6 +34,9 @@ Constraints:
 """
 
 
+# Sliding window
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         l = r = 0
@@ -54,7 +57,71 @@ class Solution:
 
         return res
 
+# Sliding window
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        left = 0
+        num_whites = 0
+        num_recolors = float("inf")
 
+        # Move right pointer
+        for right in range(len(blocks)):
+
+            # Increment num_whites if block at right pointer is white
+            if blocks[right] == "W":
+                num_whites += 1
+
+            # k consecutive elements are found
+            if right - left + 1 == k:
+
+                # Update minimum
+                num_recolors = min(num_recolors, num_whites)
+
+                # Decrement num_whites if block at left pointer is white
+                if blocks[left] == "W":
+                    num_whites -= 1
+
+                # Move left pointer
+                left += 1
+
+        return num_recolors
+
+# QUEUE
+# Time Complexity: O(n)
+# Space Complexity: O(m) k elements
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        block_queue = deque()
+        num_whites = 0
+
+        # Initiate queue with first k values
+        for i in range(k):
+            current_char = blocks[i]
+            if current_char == "W":
+                num_whites += 1
+            block_queue.append(current_char)
+
+        # Set initial minimum
+        num_recolors = num_whites
+
+        for i in range(k, len(blocks)):
+
+            # Remove front element from queue and update current number of white blocks
+            if block_queue.popleft() == "W":
+                num_whites -= 1
+
+            # Add current element to queue and update current number of white blocks
+            current_char = blocks[i]
+            if current_char == "W":
+                num_whites += 1
+            block_queue.append(current_char)
+
+            # Update minimum
+            num_recolors = min(num_recolors, num_whites)
+
+        return num_recolors
         
 solution = Solution()
 
@@ -80,3 +147,4 @@ update the answer and
 
 
 """
+
