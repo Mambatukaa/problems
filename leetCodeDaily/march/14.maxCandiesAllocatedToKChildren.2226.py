d880ea5bf7a702ba3c1d7b8b348bc32cc3bb8d3d
mambatukaa <sbatuka9@gmail.com>
2025-03-14 11:31:48 -0400

    add solution using binary search

---
+++ leetCodeDaily/march/14.maxCandiesAllocatedToKChildren.2226.py
@@ -0,0 +1,68 @@
+""""
+
+You are given a 0-indexed integer array candies. Each element in the array denotes a pile of candies of size
+candies [il. You can divide each pile into any number of sub piles, but you cannot merge two piles together.
+You are also given an integer k. You should allocate piles of candies to k children such that each child gets
+the same number of candies. Each child can be allocated candies from only one pile of candies and some
+piles of candies may go unused.
+Return the maximum number of candies each child can get.
+
+Example 1:
+    Input: candies = [5,8,6], k = 3
+    Output: 5
+    Explanation: We can divide candies[1] into 2 piles of size 5 and 3, and candies[2]
+    into 2 piles of size 5 and 1. We now have five piles of candies of sizes 5, 5, 3,
+    5, and 1. We can allocate the 3 piles of size 5 to 3 children. It can be proven
+    that each child cannot receive more than 5 candies.
+
+Example 2:
+    Input: candies = [2,51, k = 11
+    Output: 0
+    Explanation: There are 11 children but only 7 candies in total, so it is
+    impossible to ensure each child receives at least one candy. Thus, each child gets
+    no candy and the answer is 0.
+
+Constraints:
+• 1 <= candies.length <= 105
+• 1 <= candies [i] <= 107
+• 1 <= k <= 1012
+
+
+"""
+
+# Time Complexity: O(n * log(n))
+# Space Complexity: O(1)
+class Solution:
+    def maximumCandies(self, candies, k: int) -> int:
+        left = 1
+        right = max(candies)
+
+        res = 0
+
+        while left <= right:
+            mid = (right - left) // 2 + left
+            ctr = 0
+
+            for candy in candies:
+                ctr += candy // mid
+
+            if ctr < k:
+                right = mid - 1
+            else:
+                res = max(res, mid)
+                left = mid + 1
+
+        return res
+
+
+solution = Solution()
+candies = [5,8,6]
+candies = [4,9,6]
+candies = [4,7,5]
+k = 4
+candies = [2,5]
+k = 11
+candies = [1]
+k = 1
+
+print("res:", solution.maximumCandies(candies, k))
