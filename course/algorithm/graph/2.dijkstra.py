09a431922d5470683b93771226bd23b1118c454c
mambatukaa <sbatuka9@gmail.com>
2025-03-19 22:28:49 -0700

    up

--- course/algorithm/graph/2.dijkstra.py
+++ course/algorithm/graph/2.dijkstra.py
@@ -3,7 +3,6 @@ import heapq
 # Can't deal with negative cycle
 
 class WeightedNode:
-
   def __init__(self, name, index):
     self.name = name
     self.index = index
@@ -31,7 +30,6 @@ class WeightedGraph:
 
     for node in self.nodeList:
       heapq.heappush(q, node)
-
 
     while q:
       heapq.heapify(q)
@@ -66,31 +64,90 @@ class WeightedGraph:
     first.weightMap[second] = d
 
 
-nodeA = WeightedNode("A", 0)
-nodeB = WeightedNode("B", 1)
-nodeC = WeightedNode("C", 2)
-nodeD = WeightedNode("D", 3)
-nodeE = WeightedNode("E", 4)
-nodeF = WeightedNode("F", 5)
-nodeG = WeightedNode("G", 6)
+node1 = WeightedNode("1", 1)
+node2 = WeightedNode("2", 2)
+node3 = WeightedNode("3", 3)
+node4 = WeightedNode("4", 4)
 
-nodeList = [nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG]
+
+node5 = WeightedNode("5", 5)
+node6 = WeightedNode("6", 6)
+node7 = WeightedNode("7", 7)
+node8 = WeightedNode("8", 8)
+
+
+node9 = WeightedNode("9", 9)
+node10 = WeightedNode("10", 10)
+node11 = WeightedNode("11", 11)
+node12 = WeightedNode("12", 12)
+
+node13 = WeightedNode("13", 13)
+node14 = WeightedNode("14", 14)
+node15 = WeightedNode("15", 15)
+node16 = WeightedNode("16", 16)
+node17 = WeightedNode("17", 17)
+
+
+
+nodeList = [node1, node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14, node15, node16, node17]
 
 newGraph = WeightedGraph(nodeList)
 
-newGraph.addWeightedEdge(0, 1, 2)
-newGraph.addWeightedEdge(0, 2, 5)
-newGraph.addWeightedEdge(1, 2, 6)
+newGraph.addWeightedEdge(1, 2, 3)
+newGraph.addWeightedEdge(1, 3, 4)
+newGraph.addWeightedEdge(1, 4, 2)
+newGraph.addWeightedEdge(1, 5, 5)
 
-newGraph.addWeightedEdge(1, 3, 1)
-newGraph.addWeightedEdge(1, 4, 3)
+newGraph.addWeightedEdge(2, 3, 4)
+newGraph.addWeightedEdge(2, 6, 2)
+newGraph.addWeightedEdge(2, 9, 6)
 
-newGraph.addWeightedEdge(2, 5, 8)
 
-newGraph.addWeightedEdge(3, 4, 4)
+newGraph.addWeightedEdge(3, 4, 1)
+newGraph.addWeightedEdge(3, 6, 3)
+newGraph.addWeightedEdge(3, 7, 5)
+newGraph.addWeightedEdge(3, 8, 4)
 
-newGraph.addWeightedEdge(4, 6, 9)
 
-newGraph.addWeightedEdge(5, 6, 7)
+newGraph.addWeightedEdge(4, 5, 2)
+newGraph.addWeightedEdge(4, 7, 4)
 
-newGraph.dijkstra(nodeA)
+
+newGraph.addWeightedEdge(5, 7, 3)
+newGraph.addWeightedEdge(5, 14, 6)
+
+
+
+newGraph.addWeightedEdge(6, 9, 3)
+newGraph.addWeightedEdge(6, 8, 3)
+
+
+newGraph.addWeightedEdge(7, 8, 3)
+newGraph.addWeightedEdge(7, 10, 2)
+
+newGraph.addWeightedEdge(8, 11, 2)
+newGraph.addWeightedEdge(8, 12, 4)
+
+newGraph.addWeightedEdge(9, 11, 2)
+newGraph.addWeightedEdge(9, 13, 4)
+
+newGraph.addWeightedEdge(10, 12, 2)
+newGraph.addWeightedEdge(10, 14, 3)
+
+newGraph.addWeightedEdge(11, 12, 5)
+newGraph.addWeightedEdge(11, 13, 1)
+newGraph.addWeightedEdge(11, 15, 2)
+
+newGraph.addWeightedEdge(12, 15, 3)
+newGraph.addWeightedEdge(12, 16, 1)
+
+
+newGraph.addWeightedEdge(13, 15, 3)
+
+newGraph.addWeightedEdge(14, 16, 7)
+
+newGraph.addWeightedEdge(15, 17, 5)
+
+newGraph.addWeightedEdge(16, 17, 3)
+
+newGraph.dijkstra(node1)
