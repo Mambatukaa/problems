2cd60f54b32171fd6a9f5d0f2ca7822eb4028d33
mambatukaa <sbatuka9@gmail.com>
2025-04-27 11:53:31 -0700

    add solution using two pointers

---
+++ leetCodeDaily/april/27.countSubarraysOfLengthThreeWithACondition.3392.py
@@ -0,0 +1,54 @@
+""""
+Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,4,1]
+
+Output: 1
+
+Explanation:
+
+Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
+
+Example 2:
+
+Input: nums = [1,1,1]
+
+Output: 0
+
+Explanation:
+
+[1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
+
+ 
+
+Constraints:
+
+3 <= nums.length <= 100
+-100 <= nums[i] <= 100
+
+"""
+
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        l = 0
+        n = len(nums)
+
+        res = 0
+
+        for r in range(2, n):
+            left = nums[l]
+            mid = nums[l + 1]
+            right = nums[r]
+
+            if mid / 2 == left + right:
+                res += 1
+
+            l += 1
+
+        return res
+
+        
