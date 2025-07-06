bec67130acdeb926b2cb6b43f6a3dba018e30efc
mambatukaa <sbatuka9@gmail.com>
2025-07-06 13:52:29 -0700

    up

--- design_patterns/creational/2.abstractFactory/abstractFactory.py
+++ design_patterns/creational/2.abstractFactory/abstractFactory.py
@@ -5,6 +5,10 @@ There are some interfaces or abstract classes, each having multiple concrete imp
 There’s an abstract object (or an interface), known as a Creator, or a Factory that returns abstract versions of Target Objects. Each Target Object type has a creation method inside the Factory object associated with it.
 There are multiple concrete implementations of the Factory object, each returning a specific set of the Target Object implementations.
 When we need to return a specific set of the Target Object implementations, we initialize a specific implementation of the Factory object and call the creation methods on it.
+
+
+
+Family related objects in one go
 """
 
 
