8b2916b73576dee7f0ebdbc545139d614299e405
mambatukaa <sbatuka9@gmail.com>
2025-07-18 22:37:03 -0700

    add solution using the iteration

---
+++ facebook/30days2/13.validWordAbbreviation.408.py
@@ -0,0 +1,85 @@
+""""
+A string can be abbreviated by replacing any number of non-adjacent, non-empty substrings with their lengths. The lengths should not have leading zeros.
+
+For example, a string such as "substitution" could be abbreviated as (but not limited to):
+
+"s10n" ("s ubstitutio n")
+"sub4u4" ("sub stit u tion")
+"12" ("substitution")
+"su3i1u2on" ("su bst i t u ti on")
+"substitution" (no substrings replaced)
+The following are not valid abbreviations:
+
+"s55n" ("s ubsti tutio n", the replaced substrings are adjacent)
+"s010n" (has leading zeros)
+"s0ubstitution" (replaces an empty substring)
+Given a string word and an abbreviation abbr, return whether the string matches the given abbreviation.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: word = "internationalization", abbr = "i12iz4n"
+Output: true
+Explanation: The word "internationalization" can be abbreviated as "i12iz4n" ("i nternational iz atio n").
+
+Example 2:
+
+Input: word = "apple", abbr = "a2e"
+Output: false
+Explanation: The word "apple" cannot be abbreviated as "a2e".
+ 
+
+Constraints:
+
+1 <= word.length <= 20
+word consists of only lowercase English letters.
+1 <= abbr.length <= 10
+abbr consists of lowercase English letters and digits.
+All the integers in abbr will fit in a 32-bit integer.
+
+
+
+
+
+word = "internationalization", abbr = "i12iz4n"
+"""
+
+class Solution:
+    # Time Complexity: O(n) abbr
+    # Space Complexity: O(m)
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(word) and j < len(abbr):
+            if word[i] == abbr[j]:
+                i += 1
+                j += 1
+            elif abbr[j].isalpha() or abbr[j] == "0":
+                return False
+            else:
+                sublen = 0
+                while j < len(abbr) and abbr[j].isdigit():
+                    sublen = sublen * 10 + int(abbr[j])
+                    j += 1
+                i += sublen
+        return len(word) == i and len(abbr) == j
+
+
+        return True
+ 
+
+
+
+word = "internationalization"
+abbr = "i12iz4n"
+word = "apple"
+abbr = "a2e"
+
+solution = Solution()
+print("res:", solution.validWordAbbreviation(word, abbr))
+
+
+
