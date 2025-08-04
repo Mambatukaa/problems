cc9e683dc9ba5db07bba3b133861b88361d2272d
mambatukaa <sbatuka9@gmail.com>
2025-08-04 09:43:15 -0700

    add solution reversing

---
+++ facebook/30days2/55.palindromeNumber.9.py
@@ -0,0 +1,55 @@
+""""
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+"""
+
+
+
+
+
+# REverse half
+# Time Complexity: O(n / 2)
+# Space Complexity: O(1)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        reversed_num = 0
+
+        while x > reversed_num:
+            last_digit = x % 10
+
+            reversed_num = reversed_num * 10 + last_digit
+
+            x //= 10
+
+        return reversed_num == x or x == reversed_num // 10
+
+
+
