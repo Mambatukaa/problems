32d73e0c92677c7e3ec045f198db77038ccd9653
mambatukaa <sbatuka9@gmail.com>
2025-08-14 10:04:26 -0700

    add solution using fast and slow pointers

---
+++ facebook/30days2/63.middleOfTheLinkedList.876.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+        
+
