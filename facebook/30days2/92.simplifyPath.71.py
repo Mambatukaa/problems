4c32f65b60f93dbcca17605e9b2662c6fcf0f84c
mambatukaa <sbatuka9@gmail.com>
2025-09-01 10:37:21 -0700

    add solution with variant

---
+++ facebook/30days2/92.simplifyPath.71.py
@@ -0,0 +1,123 @@
+
+"""
+You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+
+The rules of a Unix-style file system are as follows:
+
+A single period '.' represents the current directory.
+A double period '..' represents the previous/parent directory.
+Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+The simplified canonical path should follow these rules:
+
+The path must start with a single slash '/'.
+Directories within the path must be separ from the root directory is not possible.
+
+Example 5:
+
+Input: path = "/.../a/../b/c/../d/./"
+
+Output: "/.../b/d"
+
+Explanation:
+
+"..." is a valid name for a directory in this problem.
+
+ 
+
+Constraints:
+
+1 <= path.length <= 3000
+path consists of English letters, digits, period '.', slash '/' or '_'.
+path is a valid absolute Unix path.ated by exactly one slash '/'.
+The path must not end with a slash '/', unless it is the root directory.
+The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+Return the simplified canonical path.
+
+ 
+
+Example 1:
+
+Input: path = "/home/"
+
+Output: "/home"
+
+Explanation:
+
+The trailing slash should be removed.
+
+Example 2:
+
+Input: path = "/home//foo/"
+
+Output: "/home/foo"
+
+Explanation:
+
+Multiple consecutive slashes are replaced by a single one.
+
+Example 3:
+
+Input: path = "/home/user/Documents/../Pictures"
+
+Output: "/home/user/Pictures"
+
+Explanation:
+
+A double period ".." refers to the directory up a level (the parent directory).
+
+Example 4:
+
+Input: path = "/../"
+
+Output: "/"
+
+Explanation:
+
+Going one level up
+
+
+
+
+"""
+
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        res = []
+        path = path.split("/")
+
+        for p in path:
+            if p == "." or not p:
+                continue
+            elif p == "..":
+                if res:
+                    res.pop()
+            else:
+                res.append(p)
+
+        return "/" + "/".join(res)
+
+    def simplifyPathII(self, cwd: str, cd: str) -> str:
+        if not cd:
+            return cwd
+        
+        if cd[0] == "/":
+            path = cd
+        else:
+            path = cwd + cd
+
+        res = []
+        path = path.split("/")
+
+        for p in path:
+            if p == "." or not p:
+                continue
+            elif p == "..":
+                if res:
+                    res.pop()
+            else:
+                res.append(p)
+
+        return "/" + "/".join(res)
