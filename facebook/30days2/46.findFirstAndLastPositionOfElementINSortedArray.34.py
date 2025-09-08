e0965a6a84dbc6a1573fadc59a0815a098f331c7
mambatukaa <sbatuka9@gmail.com>
2025-09-08 08:04:13 -0700

    add solution using bs

--- facebook/30days2/46.findFirstAndLastPositionOfElementINSortedArray.34.py
+++ facebook/30days2/46.findFirstAndLastPositionOfElementINSortedArray.34.py
@@ -179,3 +179,47 @@ if target is not in nums return [-1, -1]
 
 
 """
+
+
+
+# Variant Find unique elements count
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+
+        n = len(nums)
+        i = 0
+
+        res = 0
+
+        while i < n:
+            target = nums[i]
+            l = 0
+            r = n - 1
+
+            # upper bound
+            while l <= r:
+                mid = (r - l) // 2 + l
+
+                if nums[mid] <= target:
+                    # go right
+                    l = mid + 1
+                else:
+                    # go left
+                    r = mid - 1
+
+            res += 1
+            i = r + 1
+
+
+        return res
+
+"""
+
+t = 5
+
+    L
+    M
+  R
+5 5 8
+
+"""
