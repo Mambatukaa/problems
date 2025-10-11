04ed0779283b8df3a840450c814f7591593b8113
mambatukaa <sbatuka9@gmail.com>
2025-10-11 09:21:09 -0700

    update

--- leetCodeDaily/october/14.adjIncreasingSubArraysDetectionI.3349.e.py
+++ leetCodeDaily/october/14.adjIncreasingSubArraysDetectionI.3349.e.py
@@ -38,6 +38,8 @@ Constraints:
 
 
 """
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
         n = len(nums)
