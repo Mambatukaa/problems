a6444984848316f8f701c9c7894ef5a524de909b
mambatukaa <sbatuka9@gmail.com>
2025-10-17 09:09:22 -0700

    Up

--- facebook/30days2/83.findPivotIndex.724.py
+++ facebook/30days2/83.findPivotIndex.724.py
@@ -144,3 +144,15 @@ class Solution:
                 return i
         
         return -1
+
+
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        S = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (S - leftsum - x):
+                return i
+            leftsum += x
+        return -1
