a07bc367eb21fb1aed394bfbfcb4c592894e5eac
mambatukaa <sbatuka9@gmail.com>
2025-11-22 14:05:11 -0800

    add solution using map:

---
+++ microsoft/30days/4.groupAnagrams.49.m.py
@@ -0,0 +1,55 @@
+""""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+
+# Time Complexity: O(N * K) N = length of strs, K is the max length of string S
+# Space Complexity: O(N * K)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        dic = defaultdict(list)
+
+        for s in strs:
+            ctr = [0] * 26
+
+            for l in s:
+                ctr[ord("a")-ord(l)] += 1
+
+            dic[tuple(ctr)].append(s)
+
+        return list(dic.values())
+
+
