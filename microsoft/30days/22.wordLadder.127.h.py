7a4cbfdc3ec7e34ff9aac720f08ef538d6f7ca8b
mambatukaa <sbatuka9@gmail.com>
2025-12-05 09:55:53 -0800

    add shorter solution

--- microsoft/30days/22.wordLadder.127.h.py
+++ microsoft/30days/22.wordLadder.127.h.py
@@ -144,3 +144,38 @@ class Solution:
                         q.append([new_word.copy(), step + 1])
 
         return 0
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # edge case
+        if endWord not in wordList:
+            return 0
+
+        nei = defaultdict(set)
+        wordList.append(beginWord)
+
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i + 1:]
+                nei[pattern].add(word)
+
+        q = deque([beginWord])
+        seen = set()
+        res = 1
+
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+
+                for i in range(len(word)):
+                    pattern = word[:i] + "*" + word[i + 1:]
+
+                    for neiWord in nei[pattern]:
+                        if neiWord not in seen:
+                            q.append(neiWord)
+                    nei[pattern] = set()
+
+            res += 1
+
+        return 0
