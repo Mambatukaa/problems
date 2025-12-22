2f393c62d2459cd87e52d53ce0cb27cb9d09a937
mambatukaa <sbatuka9@gmail.com>
2025-12-21 17:50:08 -0800

    recursive solution

--- neetCode250/5.linkedList/1.mergeTwoSortedLists.21.e.py
+++ neetCode250/5.linkedList/1.mergeTwoSortedLists.21.e.py
@@ -60,3 +60,27 @@ class Solution:
 
         
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# TC: O(N + M)
+# SC: O(N + M)
+# Recursive
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+        
