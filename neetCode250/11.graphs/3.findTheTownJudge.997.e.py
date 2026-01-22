4eb0e26236aa9ca9e71d1e71335ce3f05b084cec
mambatukaa <sbatuka9@gmail.com>
2026-01-22 17:11:18 -0600

    add complexity

--- neetCode250/11.graphs/3.findTheTownJudge.997.e.py
+++ neetCode250/11.graphs/3.findTheTownJudge.997.e.py
@@ -36,7 +36,7 @@ ai != bi
 1 <= ai, bi <= n
 """
 
-# SC: O(n)
+# SC: O(E) edges
 # TC: O(n)
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
@@ -54,3 +54,22 @@ class Solution:
             if indegree[i] == N - 1 and outdegree[i] == 0:
                 return i
         return -1 
+
+
+# Time Complexity: O(E) E - edges 
+# Space Complexity: O(N)
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if len(trust) < N - 1:
+            return -1
+    
+        trust_scores = [0] * (N + 1)
+    
+        for a, b in trust:
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
+        
+        for i, score in enumerate(trust_scores[1:], 1):
+            if score == N - 1:
+                return i
+        return -1
