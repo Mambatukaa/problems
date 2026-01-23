d3bf3c459d5768a832e0a73003ae7684554270e4
mambatukaa <sbatuka9@gmail.com>
2026-01-23 16:22:21 -0600

    add solution using iteration

---
+++ neetCode250/2.TwoPointers/3.mergeStringsAlternately.1768.e.py
@@ -0,0 +1,73 @@
+""""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+"""
+
+
+# TC: O(N + M)
+# SC: O(N + M)
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        m = len(word1)
+        n = len(word2)
+        i = 0
+        j = 0
+        result = []
+
+        while i < m or j < n:
+            if i < m:
+                result += word1[i]
+                i += 1
+            if j < n:
+                result += word2[j]
+                j += 1
+
+        return "".join(result)
+
+# TC: O(N + M)
+# SC: O(N + M)
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = []
+        n = max(len(word1), len(word2))
+        for i in range(n):
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+
+        return "".join(result)
