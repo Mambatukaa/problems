b3ace675c03ec3add00cd03eddcfbc8b57c48db9
mambatukaa <sbatuka9@gmail.com>
2026-02-17 21:16:39 -0600

    solution using ll

---
+++ neetCode250/1.arrayNHashing/10.designHashMap.706.e.py
@@ -0,0 +1,125 @@
+""""
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+ 
+
+Example 1:
+
+Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+Explanation
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+ 
+
+Constraints:
+
+0 <= key, value <= 106
+At most 104 calls will be made to put, get, and remove.
+
+"""
+
+
+class Node:
+    def __init__(self, key, value, nextNode=None):
+        self.key = key
+        self.value = value
+        self.next = nextNode
+
+class Bucket:
+    def __init__(self):
+        self.head = Node(0, 0)
+
+    def insert(self, key, value):
+        node = self.exists(key)
+
+        if node:
+            # change the value
+            node.value = value
+            
+            return
+        
+        newNode = Node(key, value, self.head.next)
+        self.head.next = newNode
+
+    def delete(self, key):
+        prev = self.head
+        curr = self.head.next
+
+        while curr:
+            if curr.key == key:
+                prev.next = curr.next
+                return
+            prev = prev.next
+            curr = curr.next
+    
+    def exists(self, key):
+        curr = self.head.next
+
+        while curr:
+            if curr.key == key:
+                # exists
+                return curr
+
+            curr = curr.next
+        
+        return None
+
+
+
+
+    
+class MyHashMap:
+
+    def __init__(self):
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+
+    def _hash(self, key):
+        return key % self.keyRange
+
+    def put(self, key: int, value: int) -> None:
+        bucketIndex = self._hash(key)
+
+        self.bucketArray[bucketIndex].insert(key, value)
+
+    def get(self, key: int) -> int:
+        bucketIndex = self._hash(key)
+
+        node = self.bucketArray[bucketIndex].exists(key)
+
+        if node:
+            return node.value
+        
+        return -1
+
+
+    def remove(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+
+        self.bucketArray[bucketIndex].delete(key)
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
