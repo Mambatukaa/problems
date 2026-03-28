1b8b4535b9d6868a8066a8a5f861b6ce789793aa
mambatukaa <sbatuka9@gmail.com>
2026-03-28 14:47:32 -0400

    add iterative and recursive solution

--- neetCode250/7.trees/8.sameTree.100.e.py
+++ neetCode250/7.trees/8.sameTree.100.e.py
@@ -47,4 +47,36 @@ class Solution:
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 
+        queue = deque([[p, q]])
+
+        while queue:
+            p, q = queue.popleft()
+
+            if not p and not q:
+                continue
+            
+            if not p:
+                return False
+            
+            if not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            queue.append([p.left, q.left])
+            queue.append([p.right, q.right])
+        
+        return True
+
+        
+        
