8c17cbc19386fe43abbc90955fa1fe37983f19bd
mambatukaa <sbatuka9@gmail.com>
2026-04-09 09:02:14 -0500

    add dfs solution

--- neetCode250/7.tree/13.BTLevelOrderTraversal.102.m.py
+++ neetCode250/7.tree/13.BTLevelOrderTraversal.102.m.py
@@ -59,3 +59,41 @@ class Solution:
             res.append(sub)
         
         return res
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+
+        res = []
+
+        def dfs(root, level):
+            if not root:
+                return
+
+            if len(res) <= level:
+                res.append([])
+
+            res[level].append(root.val)
+
+            dfs(root.left, level + 1)
+            dfs(root.right, level + 1)
+
+        
+
+        dfs(root, 0)
+        
+        return res
+
+
