115b5c9d7a9849ca0b52402b8de8bcb5abe8d12d
mambatukaa <sbatuka9@gmail.com>
2026-04-27 15:16:18 -0500

    add solution using bfs

--- neetCode250/11.graphs/6.cloneGraph.133.m.py
+++ neetCode250/11.graphs/6.cloneGraph.133.m.py
@@ -94,3 +94,39 @@ class Solution:
 
 
         
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from collections import deque
+
+
+# TC: O(N + M)
+# SC: O(N)
+class Solution:
+
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return node
+
+        visited = {}
+
+        queue = deque([node])
+        visited[node] = Node(node.val, [])
+
+        while queue:
+            n = queue.popleft()
+
+            for neighbor in n.neighbors:
+                
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
+
+                visited[n].neighbors.append(visited[neighbor])
+
+        return visited[node]
